e5d9eb97cf4272b787dbf6d38c81cb5f7d863072
mastervalor <mouradmarzouk63@gmail.com>
2024-01-04 18:44:27 -0800

    Update csv_merge_missing.py

--- csv_merge_missing.py
+++ csv_merge_missing.py
@@ -4,6 +4,15 @@ import os
 mainFile = 'Inactive project leads'
 final = 'unowned projects'
 
+keys = []
 
-with open ('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), final), mode='a') as edit_file:
-    
+with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), final), mode='r') as edit_file:
+    csv_reader2 = csv.DictReader(edit_file)
+    for i in csv_reader2:
+        keys.append(i['Key'])
+
+    with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), mainFile), mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for i in csv_reader:
+            if i['key'] not in keys:
+                print(i['key'])
