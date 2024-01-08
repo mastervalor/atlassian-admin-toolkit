f7a4aff0c0281b00240efcfb959c5f6cbd5ac35c
mastervalor <mouradmarzouk63@gmail.com>
2024-01-08 12:56:47 -0800

    Update getProjectOwners.py

--- getProjectOwners.py
+++ getProjectOwners.py
@@ -3,13 +3,16 @@ import csv
 from call import Jira
 
 jira = Jira()
-newFile = 'full projects list'
-openFile = 'final file'
+newFile = 'final owners'
+openFile = 'unowned projects'
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
     writer = csv.writer(new_csv)
-    writer.writerow(['Name', 'Key', 'Project Type', 'Owner'])
+    writer.writerow(['Owner'])
     with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), openFile), mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for i in csv_reader:
-            jira.get_project(i['Key'])
+            owner = jira.project_owner(i['Key'])
+            writer.writerow([owner[0]])
+            print(owner[0])
+            
