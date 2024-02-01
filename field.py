db4113f6d8df7fe8cb3463f54b0ca4057517cd7d
mastervalor <mouradmarzouk63@gmail.com>
2024-02-01 13:16:51 -0800

    Update field.py

--- field.py
+++ field.py
@@ -4,36 +4,39 @@ import json
 import re
 from tabulate import tabulate
 
-url = "https://lucidmotors.atlassian.net/rest/api/3/field"
+url = "https://jira.robot.car/rest/api/2/auditing/record"
 
 headers = {
     "Accept": "application/json"
 }
-
+params = {
+    "filter": "customfield_11300"
+}
 response = requests.request(
     "GET",
     url,
     headers=headers,
-    auth=auth
+    auth=auth,
+    params=params
 )
 
 response = json.loads(response.text)
 fields = []
+#
+# for i in response:
+#     try:
+#         if i['schema']:
+#             if ('com.atlassian.jira.toolkit:message' or 'com.atlassian.servicedesk') in i['schema']['custom']:
+#                 continue
+#             else:
+#                 fields.append(
+#                     [i['name'], i['schema']['custom'].replace('com.atlassian.jira.plugin.system.customfieldtypes:', '')])
+#     except KeyError:
+#         continue
+#
+# print(tabulate(fields, headers=["name", "type"]))
 
-for i in response:
-    try:
-        if i['schema']:
-            if ('com.atlassian.jira.toolkit:message' or 'com.atlassian.servicedesk') in i['schema']['custom']:
-                continue
-            else:
-                fields.append(
-                    [i['name'], i['schema']['custom'].replace('com.atlassian.jira.plugin.system.customfieldtypes:', '')])
-    except KeyError:
-        continue
-
-print(tabulate(fields, headers=["name", "type"]))
-
-# print(json.dumps(response, sort_keys=True, indent=4, separators=(",", ": ")))
+print(json.dumps(response, sort_keys=True, indent=4, separators=(",", ": ")))
 # output = json.loads(response.text)
 # print(response[0]['schema']['type'])
 # for i in response:
