f265e63e8bd869721a80b5c91acd89c094798cf6
mastervalor <mouradmarzouk63@gmail.com>
2024-02-01 11:19:14 -0800

    class test

--- test5.py
+++ test5.py
@@ -1,28 +1,8 @@
-import requests
-import json
-from auth import auth
+from logic.groups_users_logic import GroupsUsers
 
-url = "https://api.atlassian.com/ex/confluence/8c456a13-53e0-4a30-8cbd-3edd74f85b8c/wiki/rest/api/space/homologation?expand=permissions"
+groups = GroupsUsers()
 
-headers = {
-    "Accept": "application/json",
-}
+members = groups.get_group_members_with_status("jira-new-hires", True)
 
-response = json.loads(requests.request(
-    "GET",
-    url,
-    headers=headers,
-    auth=auth
-).text)
-#
-# for a in responseg["permissions"]:
-#     keys.append(a['subjects']['group']['results'][0]['name'])
-#
-#     print(keys)
-for a in response["permissions"]:
-    try:
-        print(a['subjects']['group']['results'][0]['name'])
-    except KeyError:
-        continue
- # print(a['subjects']['group']['results'][0]['name'], i['name'])
-# print(json.dumps(response, sort_keys=True, indent=4, separators=(",", ": ")))
+for i in members:
+    print(i)
