c3c0de838418aa49cfa19cdb44dab37da0d65d59
mastervalor <mouradmarzouk63@gmail.com>
2024-02-12 12:39:41 -0800

    created test

--- test5.py
+++ test5.py
@@ -1,9 +1,17 @@
-from call import Jira
+import os, csv
 from logic.groups_users_logic import GroupsUsers
+from logic.project_logic import Projects
 
 groups = GroupsUsers()
+projects = Projects()
 
-groups.remove_all_group_members("jira-new-hires")
+# groups.remove_all_group_members("jira-new-hires")
 
+openFile = 'projects'
 
-# print(members, sort_keys=True, indent=4, separators=(",", ": "))
+with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), openFile), mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        project = projects.get_project_type(row['Project Key'])
+        print(project)
+# print(json.dumps(project, sort_keys=True, indent=4, separators=(",", ": ")))
