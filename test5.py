7e7c1ca57e8c012b612aaa601e6b80b8e8f7edd7
mastervalor <mouradmarzouk63@gmail.com>
2024-04-16 17:27:56 -0700

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,17 +1,22 @@
 import os, csv
 from logic.groups_users_logic import GroupsUsers
 from logic.project_logic import Projects
+from logic.field_logic import Fields
 
 groups = GroupsUsers()
 projects = Projects()
+fields = Fields()
 
 # groups.remove_all_group_members("jira-new-hires")
+#
+# openFile = 'projects'
+#
+# with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), openFile), mode='r') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+#     for row in csv_reader:
+#         project = projects.get_project_type(row['Project Key'])
+#         print(project)
 
-openFile = 'projects'
 
-with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), openFile), mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        project = projects.get_project_type(row['Project Key'])
-        print(project)
+fields.field_metrics()
 # print(json.dumps(project, sort_keys=True, indent=4, separators=(",", ": ")))
