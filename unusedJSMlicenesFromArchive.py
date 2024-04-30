f2653bd85f1b3f002653812cee9664e192935ad6
mastervalor <mouradmarzouk63@gmail.com>
2024-04-30 13:56:52 -0700

    Update unusedJSMlicenesFromArchive.py

--- unusedJSMlicenesFromArchive.py
+++ unusedJSMlicenesFromArchive.py
@@ -8,6 +8,7 @@ results = projects.get_archived_projects()
 jsm_projects = []
 jsm_users = []
 users_groups = []
+final_user_groups = []
 
 for project in results:
     if project['projectTypeKey'] == 'service_desk':
@@ -20,10 +21,12 @@ for project in jsm_projects:
     for user in users:
         if user not in jsm_users:
             jsm_users.append({user: group})
+            final_user_groups.append({user: ''})
 
 for user_group in jsm_users:
     for user, group in user_group.items():
         looking_groups = groups.user_groups(user)
         for looking_group in looking_groups:
             if looking_group.endswith("-agent") and looking_group != group:
-                print(f"{user}: {group}")
+                if looking_group not in final_user_groups[user]:
+                    final_user_groups[user].add(looking_group)
