6afb39093b82912fa34e351ebce17e189aa5a5dc
mastervalor <mouradmarzouk63@gmail.com>
2024-04-30 14:57:12 -0700

    Update unusedJSMlicenesFromArchive.py

--- unusedJSMlicenesFromArchive.py
+++ unusedJSMlicenesFromArchive.py
@@ -6,17 +6,10 @@ projects = Projects()
 groups = GroupsUsers()
 results = projects.get_archived_projects()
 jsm_projects = []
-jsm_users = []
-users_groups = []
-final_user_groups = []
 
 for project in results:
     if project['projectTypeKey'] == 'service_desk':
         jsm_projects.append(project['key'])
-
-for project in jsm_projects:
-    group = f"app-jira-{project}-agent"
-    users = groups.get_group_members_with_status(group)
 
 agent_groups_by_user = []
 
