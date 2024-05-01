d50113abb88bf398f92604b2622b8590d618f25e
mastervalor <mouradmarzouk63@gmail.com>
2024-05-01 11:07:25 -0700

    update the init function

--- logic/os_logic.py
+++ logic/os_logic.py
@@ -3,11 +3,12 @@ import csv
 
 
 class OS_Logic:
-    def __init__(self):
+    def __init__(self, open_file=None, write_file=None):
+        self.open_file = open_file
+        self.write_file = write_file
 
-
-    def read_file(self, open_file):
-        file_path = '/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), open_file)
+    def read_file(self):
+        file_path = '/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), self.open_file)
         try:
             with open(file_path, mode='r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
