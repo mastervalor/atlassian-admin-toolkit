202638871c53aa8470cdb492212d720f58474607
mastervalor <mouradmarzouk63@gmail.com>
2024-05-01 11:06:28 -0700

    Update os_logic.py

--- logic/os_logic.py
+++ logic/os_logic.py
@@ -3,8 +3,15 @@ import csv
 
 
 class OS_Logic:
-    def __init__(self, open_file, close_file):
-        self.open_file = open_file
-        self.close_file = close_file
+    def __init__(self):
 
-    
+
+    def read_file(self, open_file):
+        file_path = '/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), open_file)
+        try:
+            with open(file_path, mode='r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                return list(csv_reader)
+        except FileNotFoundError:
+            print(f"File '{file_path}' not found.")
+            return []
