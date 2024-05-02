84190b32fb64b0a43fc871c9405f5384af6eceee
mastervalor <mouradmarzouk63@gmail.com>
2024-05-02 15:33:20 -0700

    Update getUsersStatus.py

--- getUsersStatus.py
+++ getUsersStatus.py
@@ -6,12 +6,15 @@ os_logic = OSLogic(open_file='jsm users')
 groups = GroupsUsers()
 jira = Jira()
 
-app_jira_agent_license = []
-jira_servicedesk_users = []
-it_Operations = []
-jira_administrators = []
+app_jira_agent_license = jira.group_members('app-jira-agent-license')
+jira_servicedesk_users = jira.group_members('jira-servicedesk-users')
+it_Operations = jira.group_members('IT Operations')
+jira_administrators = jira.group_members('jira-administrators')
+servicedesk = jira.group_members('servicedesk')
 
 
 file = os_logic.read_file()
 for i in file:
     print(i['Username'])
+
+#next loop through the sheet and get all their status and check what group they are from.
