253cedc92267df3d541a6ab1466e529b532def76
mastervalor <mouradmarzouk63@gmail.com>
2024-05-02 15:16:18 -0700

    starting to build list

--- getUsersStatus.py
+++ getUsersStatus.py
@@ -1,8 +1,16 @@
 from logic.groups_users_logic import GroupsUsers
 from logic.os_logic import OSLogic
+from calls.jira import Jira
 
 os_logic = OSLogic(open_file='jsm users')
 groups = GroupsUsers()
+jira = Jira()
+
+app_jira_agent_license = []
+jira_servicedesk_users = []
+it_Operations = []
+jira_administrators = []
+
 
 file = os_logic.read_file()
 for i in file:
