a3096eb348d53901a96455de0c53b8cee1e8f101
mastervalor <mouradmarzouk63@gmail.com>
2024-05-03 13:37:12 -0700

    fixed issue of all things getting caught as non and added write to csv

--- unusedJSMlicenesFromArchive.py
+++ unusedJSMlicenesFromArchive.py
@@ -1,17 +1,19 @@
 from logic.project_logic import Projects
 from logic.groups_users_logic import GroupsUsers
 import json
+import csv, os
 
 projects = Projects()
 groups = GroupsUsers()
 results = projects.get_archived_projects()
 jsm_projects_keys = []
+newFile = 'former jsm users'
 
 for project in results:
     if project['projectTypeKey'] == 'service_desk':
         jsm_projects_keys.append(project['key'])
 
-project_strings = [f"app-jira-{project}-agent" for project in jsm_projects_keys]
+project_strings = [f"app-jira-{project}-agent".lower() for project in jsm_projects_keys]
 
 agent_groups_by_user = []
 
@@ -28,7 +30,16 @@ for project_string in project_strings:
             else:
                 non_archived_project_groups.append(agent_group)
         # Append user dictionary with agent groups categorized by project type
-        agent_groups_by_user.append({member: {'archived_projects_agent_groups': archived_project_groups,
-                                              'non_archived_projects_agent_groups': non_archived_project_groups}})
+        agent_groups_by_user.append({'name': member, 'archived_projects_agent_groups': archived_project_groups,
+                                     'non_archived_projects_agent_groups': non_archived_project_groups})
 
-print(json.dumps(agent_groups_by_user, sort_keys=True, indent=4, separators=(",", ": ")))
+with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
+    writer = csv.writer(new_csv)
+    writer.writerow(['Name', 'Archived_projects_agent_groups', 'Non_archived_projects_agent_groups'])
+
+    for agent in agent_groups_by_user:
+        writer.writerow([agent['name'], agent['archived_projects_agent_groups'],
+                         agent['non_archived_projects_agent_groups']])
+
+
+# print(json.dumps(agent_groups_by_user, sort_keys=True, indent=4, separators=(",", ": ")))
