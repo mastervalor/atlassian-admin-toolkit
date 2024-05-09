91b6acf4a77f5d5181f9bce0b32f3641812220cb
mastervalor <mouradmarzouk63@gmail.com>
2024-05-09 16:49:25 -0700

    Update inactive_project_owners.py

--- inactive_project_owners.py
+++ inactive_project_owners.py
@@ -1,11 +1,11 @@
-from call import Jira
+from logic.project_logic import Projects
 import csv
 import os
 
 
-jira = Jira()
+project = Projects()
 newfile = "Inactive project leads"
-projects = jira.get_projects_with_owners()
+projects = project.get_project_owners_and_status()
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newfile), mode='w') as new_csv:
     writer = csv.writer(new_csv)
