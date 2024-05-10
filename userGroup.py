6acd0d42ac519cdb9eb5d6758837cb43dd69396e
mastervalor <mouradmarzouk63@gmail.com>
2024-05-10 14:50:06 -0700

    Update userGroup.py

--- userGroup.py
+++ userGroup.py
@@ -3,11 +3,12 @@ from logic.groups_users_logic import GroupsUsers
 
 os_logic = OSLogic(open_file='learn agents', write_file='learn agents groups')
 groups = GroupsUsers()
-
 file = os_logic.read_file()
-os_logic.write_file("learn agents", "other groups")
+
+os_logic.append_file(["learn agents", "other groups"])
 
 for name in file:
     user_group = groups.user_groups(name['LEARN agents '])
     agent_groups = [group for group in user_group if group.endswith("-agent")]
-    os_logic.write_file(name['LEARN agents '], agent_groups)
+    os_logic.append_file({"learn agents": name['LEARN agents '], "other groups": agent_groups})
+
