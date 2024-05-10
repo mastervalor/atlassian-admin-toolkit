56ed685c9f9fd76ded76150237ee7d71b84b2a4c
mastervalor <mouradmarzouk63@gmail.com>
2024-05-10 15:21:40 -0700

    made edit to make sure data is being in-taken simply

--- userGroup.py
+++ userGroup.py
@@ -1,14 +1,13 @@
 from logic.os_logic import OSLogic
 from logic.groups_users_logic import GroupsUsers
 
-os_logic = OSLogic(open_file='learn agents', write_file='learn agents groups')
+columns = ["learn agents", "other groups"]
+
+os_logic = OSLogic(open_file='learn agents', append_file='learn agents groups', columns=columns)
 groups = GroupsUsers()
 file = os_logic.read_file()
-
-os_logic.append_file(["learn agents", "other groups"])
 
 for name in file:
     user_group = groups.user_groups(name['LEARN agents '])
     agent_groups = [group for group in user_group if group.endswith("-agent")]
-    os_logic.append_file({"learn agents": name['LEARN agents '], "other groups": agent_groups})
-
+    os_logic.append_to_file([name['LEARN agents '], agent_groups])
