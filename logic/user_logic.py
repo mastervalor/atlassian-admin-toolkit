162d9c54f02b3c74b8e72e04929bc20ed8490ede
mastervalor <mouradmarzouk63@gmail.com>
2024-05-14 18:41:59 -0700

    added the user groups function

--- logic/user_logic.py
+++ logic/user_logic.py
@@ -4,3 +4,12 @@ from calls.jira import Jira
 class Users:
     def __init__(self):
         self.jira = Jira()
+
+    def user_groups(self, user):
+        groups = self.jira.get_user(user, '?expand=groups')
+        user_groups = []
+
+        for group in groups['groups']['items']:
+            user_groups.append(group['name'])
+
+        return user_groups
