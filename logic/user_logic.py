3701867e808fb320b37e16389e63d4a2e6abe5a3
mastervalor <mouradmarzouk63@gmail.com>
2024-05-14 19:02:59 -0700

    moved the function

--- logic/user_logic.py
+++ logic/user_logic.py
@@ -13,3 +13,10 @@ class Users:
             user_groups.append(group['name'])
 
         return user_groups
+
+    def get_user_status(self, user):
+        user_profile = self.jira.get_user(user)
+        if user_profile['active']:
+            return 'Active'
+        else:
+            return 'Inactive'
