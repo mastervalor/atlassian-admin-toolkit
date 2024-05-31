a46c481006e31f3ee19c79f89aaa3f3048971179
mastervalor <mouradmarzouk63@gmail.com>
2024-05-31 15:22:26 -0700

    Update create_archived_project_tickets.py

--- create_archived_project_tickets.py
+++ create_archived_project_tickets.py
@@ -3,3 +3,10 @@ from logic.ticket_logic import Tickets
 
 os_logic = OSLogic(open_file='Archived projects')
 file = os_logic.read_file()
+
+for row in file:
+    ticket_info = {'parent ticket': 'CORPENG-9593',
+                   'summary': f"Project {row['project_key']} needs to be rolled over to new archived project standards",
+                   'assignee': "msteam@praecipio.com"}
+
+    print(row)
