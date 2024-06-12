dc1d38592a1f710bf8f038f9ed4cd4eb708c156e
mastervalor <mouradmarzouk63@gmail.com>
2024-06-12 15:15:54 -0700

    added logic for calling parent page

--- logic/confluence_space_logic.py
+++ logic/confluence_space_logic.py
@@ -6,8 +6,8 @@ class Spaces:
     def __init__(self):
         self.conf = Confluence()
 
-    def create_page(self, space_key, title, content):
+    def create_page(self, space_key, title, content, ancestors):
         page_type = 'page'
-        result = self.conf.create_content(page_type, space_key, title, content)
+        result = self.conf.create_content(page_type, space_key, title, content, ancestors)
 
         return result
