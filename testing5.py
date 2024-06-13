ff7f33128c1cde9e3d541874dbe98e58f98cc47c
mastervalor <mouradmarzouk63@gmail.com>
2024-06-13 15:56:15 -0700

    Update testing5.py

--- testing5.py
+++ testing5.py
@@ -1,21 +1,26 @@
-def clockHandAngles(seconds):
-    hours = 0
-    minutes = 0
-    second = 0
-    while seconds > 0:
-        if seconds >= 3600:
-            hours += 1
-            seconds -= 3600
-        elif seconds >= 60:
-            minutes += 1
-            seconds -= 60
-        elif seconds <= 59:
-            second += 1
-            seconds -= 1
-        hourDegree = (360/12) * hours
-        minuteDegree = (360/60) * minutes
-        secondDegree = (360/60) * second
-    return f"The hands in degrees hours: {hourDegree} and minutes: {minuteDegree} and seconds: {secondDegree}"
+from bs4 import BeautifulSoup
 
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
 
-print(clockHandAngles(3674))
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
+print(soup.p)
+print(soup.a)
+
+print(soup.find_all('a'))
+print(soup.find(id='link3'))
+
+print(soup.select('p.story'))
