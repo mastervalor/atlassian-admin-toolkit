8b7d10d2d4a4cfd8cc2ff4bb4e17cab388799a3f
mastervalor <mouradmarzouk63@gmail.com>
2024-06-14 15:10:36 -0700

    Update unarchive_jira_projects.py

--- unarchive_jira_projects.py
+++ unarchive_jira_projects.py
@@ -2,5 +2,12 @@ from logic.project_logic import Projects
 from logic.os_logic import OSLogic
 
 projects = Projects()
-os_logic = OSLogic()
+os_logic = OSLogic(open_file='Archived projects')
+file = os_logic.read_file()
+project_list = []
+for project in file:
+    project_list.append(project['project_key'])
 
+responses = projects.unarchive_projects_list(project_list)
+
+print(responses)
