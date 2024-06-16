53cfd360031215d2da14d556c54be1cf86adf10b
mastervalor <mouradmarzouk63@gmail.com>
2024-06-15 23:26:09 -0700

    Update archive_jira_projects.py

--- archive_jira_projects.py
+++ archive_jira_projects.py
@@ -1,3 +1,8 @@
 from logic.project_logic import Projects
 from logic.os_logic import OSLogic
 import json
+
+projects = Projects()
+os_logic = OSLogic(open_file='Archived projects')
+file = os_logic.read_file()
+project_list = []
