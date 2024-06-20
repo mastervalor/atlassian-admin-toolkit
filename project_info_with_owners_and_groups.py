d282c2b4aebc8f35408a1307b8d1481e5866920d
mastervalor <mouradmarzouk63@gmail.com>
2024-06-20 13:43:50 -0700

    Update project_info_with_owners_and_groups.py

--- project_info_with_owners_and_groups.py
+++ project_info_with_owners_and_groups.py
@@ -3,28 +3,19 @@ from logic.os_logic import OSLogic
 from dataformating.json_formating import JSONFormating
 
 project_logic = Projects()
-os_logic = OSLogic(write_file='all projects')
-
+os_logic = OSLogic(open_file='Archived projects - projects to standerdize', write_file='all projects')
+file = os_logic.read_file()
+archived_projects = []
 projects_table = []
 
+for project in file:
+    archived_projects.append(project['project_key'])
 
 projects = project_logic.get_project_owners_and_status()
 
-# project = project.get_project_admins_group('CORPENG')
-# project = project.get_project_developer_group('CORPENG')
-
 for project in projects:
-    projects_table.append({
-        'Name': project['Project'],
-        'Key': project['Key'],
-        'Approver': project['Name'],
-        'Approver status': project['Active'],
-        'Admin group': project_logic.get_project_admins_group(project['Key']),
-        'Developer group': project_logic.get_project_developer_group(project['Key']),
-        'User group': project_logic.get_project_users_group(project['Key']),
-        'Agent group':project_logic.get_project_agents_group(project['Key']),
-        'Project type': project_logic.get_project_type(project['Key'])
-    })
+    if project['Key'] not in archived_projects:
+        projects_table.append(project_logic.build_project_table(project['Project'], project['Key'], project['Name'],
+                                                                project['Active']))
 
-JSONFormating.pretty_json(projects)
-
+JSONFormating.pretty_json(projects_table)
