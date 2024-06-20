30bef6418e4c67221dc1f21d48e527e8fc352d30
mastervalor <mouradmarzouk63@gmail.com>
2024-06-20 11:27:59 -0700

    Update json_formating.py

--- dataformating/json_formating.py
+++ dataformating/json_formating.py
@@ -3,4 +3,6 @@ import json
 class JSONFormating:
     def __init__(self, file):
         self.file = file
-    
+
+    def pretty_json(self):
+        print(json.dumps(self.file, sort_keys=True, indent=4, separators=(",", ": ")))
