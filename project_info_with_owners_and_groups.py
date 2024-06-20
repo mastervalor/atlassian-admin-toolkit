994b21cefc28adfbd0cc9efedc137b20f55dee9f
mastervalor <mouradmarzouk63@gmail.com>
2024-06-20 12:06:26 -0700

    Update project_info_with_owners_and_groups.py

--- project_info_with_owners_and_groups.py
+++ project_info_with_owners_and_groups.py
@@ -2,14 +2,29 @@ from logic.project_logic import Projects
 from logic.os_logic import OSLogic
 from dataformating.json_formating import JSONFormating
 
-project = Projects()
+project_logic = Projects()
 os_logic = OSLogic(write_file='all projects')
 
 projects_table = []
 
 
-# projects = project.get_project_owners_and_status()
+projects = project_logic.get_project_owners_and_status()
 
-project = project.get_project_admins_group('CORPENG')
-JSONFormating.pretty_json(project)
+# project = project.get_project_admins_group('CORPENG')
+# project = project.get_project_developer_group('CORPENG')
+
+for project in projects:
+    projects_table.append({
+        'Name': project['Project'],
+        'Key': project['Key'],
+        'Approver': project['Name'],
+        'Approver status': project['Active'],
+        'Admin group': project_logic.get_project_admins_group(project['Key']),
+        'Developer group': project_logic.get_project_developer_group(project['Key']),
+        'User group': project_logic.get_project_users_group(project['Key']),
+        'Agent group':project_logic.get_project_agents_group(project['Key']),
+        'Project type': project_logic.get_project_type(project['Key'])
+    })
+
+JSONFormating.pretty_json(projects)
 
