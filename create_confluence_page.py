033d8ae3429f82282e5bb8c78d64afc91dbd9c0c
mastervalor <mouradmarzouk63@gmail.com>
2024-06-20 15:22:53 -0700

    Update create_confluence_page.py

--- create_confluence_page.py
+++ create_confluence_page.py
@@ -1,20 +1,19 @@
 from logic.confluence_logic.page_logic import Pages
 from logic.os_logic import OSLogic
-from
+from project_info_with_owners_and_groups import project_info
 import html
 import logging
 
-os_logic = OSLogic(open_file='Inactive project leads')
 page = Pages()
 
-file = os_logic.read_file()
+projects = project_info()
 parent_page = '421062700'
 # test_page = '588288664'
 title = 'table create test from script three'
 space_key = 'IT'
 projects_dict = []
 
-for row in file:
+for row in projects:
     approver = row['username']
     if '[C]' in approver:
         approver = approver.replace('[C]', '')
