918eb5e50dde5aa285cca4aef25a506005a301fe
mastervalor <mouradmarzouk63@gmail.com>
2024-06-20 11:23:39 -0700

    Update project_info_with_owners_and_groups.py

--- project_info_with_owners_and_groups.py
+++ project_info_with_owners_and_groups.py
@@ -1,3 +1,14 @@
 from logic.project_logic import Projects
+from logic.os_logic import OSLogic
 
 project = Projects()
+os_logic = OSLogic(write_file='all projects')
+
+projects_table = []
+
+
+# projects = project.get_project_owners_and_status()
+
+print(project.get_project_admins_group('CORPENG'))
+
+print(json.dumps(member, sort_keys=True, indent=4, separators=(",", ": ")))
