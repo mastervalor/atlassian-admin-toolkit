90fec964adc7588cbb1dc83d647b576097d08b04
mastervalor <mouradmarzouk63@gmail.com>
2024-06-30 23:22:09 -0700

    Update page_logic_test.py

--- tests/page_logic_test.py
+++ tests/page_logic_test.py
@@ -2,7 +2,12 @@ from logic.confluence_logic.page_logic import Pages
 from dataformating.json_formating import JSONFormating
 
 page = Pages()
-j_formating = JSONFormating()
+formating = JSONFormating()
+
+def test_get_page_type(page_id):
+    page_type = page.get_page_type('355776435')
+    formating.pretty_json(page_type)
+
 
 
 
