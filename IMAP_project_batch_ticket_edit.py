33abc81d03cacf74d267754739a5e8ccb05702c9
mastervalor <mouradmarzouk63@gmail.com>
2024-07-15 16:00:04 -0700

    Update IMAP_project_batch_ticket_edit.py

--- IMAP_project_batch_ticket_edit.py
+++ IMAP_project_batch_ticket_edit.py
@@ -90,13 +90,13 @@ def add_issue_link(inward_issue_key, outward_issue_key, link_type):
     return response
 
 
-#
-# with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), tickets_file), mode='r') as csv_file:
-#     csv_reader = csv.DictReader(csv_file)
-#     for ticket in csv_reader:
-#         fields = {
-#
-#         }
+
+with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), tickets_file), mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for ticket in csv_reader:
+        fields = {
+
+        }
 
 ticket = 'IMAP-1517'
 fields_edited = {
