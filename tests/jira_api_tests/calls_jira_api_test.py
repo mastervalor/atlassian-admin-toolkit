d34c0f9f2b0d12591bb546c406817d52a38270aa
mastervalor <mouradmarzouk63@gmail.com>
2024-07-16 19:44:05 -0700

    Update calls_jira_api_test.py

--- tests/jira_api_tests/calls_jira_api_test.py
+++ tests/jira_api_tests/calls_jira_api_test.py
@@ -8,3 +8,15 @@ formating = JSONFormating()
 def test_edit_ticket(key, payload):
     edit_ticket = jira.edit_ticket(key, payload)
     print(edit_ticket)
+
+
+def test_get_ticket(key):
+    ticket = jira.get_ticket(key)
+    formating.pretty_json(ticket)
+
+
+
+# payload = {
+#     "fields":
+#         "customfield_26200": {"value": "E3"}
+#     }
