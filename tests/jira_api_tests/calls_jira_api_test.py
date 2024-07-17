cb227d0d927c70d46face32e72f0fd9c853843ea
mastervalor <mouradmarzouk63@gmail.com>
2024-07-16 19:50:34 -0700

    Update calls_jira_api_test.py

--- tests/jira_api_tests/calls_jira_api_test.py
+++ tests/jira_api_tests/calls_jira_api_test.py
@@ -1,13 +1,16 @@
 from calls.jira import Jira
 from dataformating.json_formating import JSONFormating
 
-jira = Jira()
+jira = Jira(is_staging=True)
 formating = JSONFormating()
 
 
 def test_edit_ticket(key, payload):
     edit_ticket = jira.edit_ticket(key, payload)
-    print(edit_ticket)
+    if edit_ticket.status_code == 200:
+        print(edit_ticket)
+    else:
+        print(edit_ticket.status_code, edit_ticket.text)
 
 
 def test_get_ticket(key):
@@ -16,7 +19,15 @@ def test_get_ticket(key):
 
 
 
-# payload = {
-#     "fields":
-#         "customfield_26200": {"value": "E3"}
-#     }
+payload = {
+    "fields": {
+        "customfield_26200": {
+            "value": "E3"
+        }
+    }
+}
+
+test_edit_ticket('IMAP-1524', payload)
+
+
+# test_get_ticket('IMAP-1637')
