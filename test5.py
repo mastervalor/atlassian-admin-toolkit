f80649f3d1075ce5cb81bc981975c85cbb84b5f1
mastervalor <mouradmarzouk63@gmail.com>
2024-07-22 14:39:12 -0700

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,7 +1,7 @@
 import os, csv
 from logic.jira_logic.groups_users_logic import GroupsUsers
 from logic.user_logic import Users
-from logic.project_logic import Projects
+from logic.jira_logic.project_logic import Projects
 from logic.jira_logic.field_logic import Fields
 from call import Jira
 
