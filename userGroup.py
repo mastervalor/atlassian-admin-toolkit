e348d675c4b30f999f0b96d5323a84737fb1e0b2
mastervalor <mouradmarzouk63@gmail.com>
2024-07-22 14:37:06 -0700

    Update userGroup.py

--- userGroup.py
+++ userGroup.py
@@ -1,5 +1,5 @@
 from logic.os_logic import OSLogic
-from logic.groups_users_logic import GroupsUsers
+from logic.jira_logic.groups_users_logic import GroupsUsers
 from logic.user_logic import Users
 
 columns = ["learn agents", "other groups"]
