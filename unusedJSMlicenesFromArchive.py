aed96938bd6f9621d984024e90ca25ea28a9e58a
mastervalor <mouradmarzouk63@gmail.com>
2024-07-22 14:37:03 -0700

    Update unusedJSMlicenesFromArchive.py

--- unusedJSMlicenesFromArchive.py
+++ unusedJSMlicenesFromArchive.py
@@ -1,5 +1,5 @@
 from logic.project_logic import Projects
-from logic.groups_users_logic import GroupsUsers
+from logic.jira_logic.groups_users_logic import GroupsUsers
 from logic.user_logic import Users
 import csv, os
 
