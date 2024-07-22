0f4de3de7f81000a73a88ffc592abc685e717bd7
mastervalor <mouradmarzouk63@gmail.com>
2024-07-22 15:21:12 -0700

    Update field_logic_tests.py

--- tests/jira_api_tests/field_logic_tests.py
+++ tests/jira_api_tests/field_logic_tests.py
@@ -1,5 +1,14 @@
 from logic.jira_logic.field_logic import Fields
 from dataformating.json_formating import JSONFormating
 
-jira = Fields()
+fields = Fields()
 formating = JSONFormating()
+
+
+def test_field_options(field_id, context_id):
+    field_options = fields.field_options(field_id, context_id)
+    if field_options.status_code == 200:
+        print("Successfully mad call")
+        print(field_options)
+    else:
+        print(field_options.status_code, field_options.text)
