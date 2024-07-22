a3443adb159cfa592b890d83fbd148333c8877b7
mastervalor <mouradmarzouk63@gmail.com>
2024-07-22 14:35:39 -0700

    Create field_logic.py

---
+++ logic/jira_logic/field_logic.py
@@ -0,0 +1,24 @@
+from call import Jira
+import urllib.parse
+import json
+
+
+class Fields:
+    def __init__(self):
+        self.jira = Jira()
+
+    def field_metrics(self):
+        field_metrics = []
+        fields = self.jira.all_fields()
+        for field in fields['values']:
+            if 'issuesWithValue' not in field or field['issuesWithValue'] == 0:
+                jql = f"'{field['name']}' is not EMPTY"
+                try:
+                    results = self.jira.jql("", jql)
+                    field['issuesWithValue'] = results['total']
+                except KeyError:
+                    print(field['name'], results['errorMessages'])
+                    field['notes'] = results['errorMessages']
+            field_metrics.append(field)
+            print(field)
+        return field_metrics
