05f3b189961b03d12db382ee472a371e1d1468ab
mastervalor <mouradmarzouk63@gmail.com>
2024-07-24 11:32:53 -0700

    Update IMAP_project_batch_ticket_edit.py

--- IMAP_project_batch_ticket_edit.py
+++ IMAP_project_batch_ticket_edit.py
@@ -1,12 +1,12 @@
 import requests
-import json
 import os
 import csv
 from auth import auth
 
 jira_base_url = "https://jira.robot.car/rest/api/2/"
 jira_dev_url = "https://jira-dev.robot.car/rest/api/2/"
-# fill in first string with username, and second string with token.
+# fill in first string with username, and second string with token comma seperated,
+# will need to look like this: ("username", "token").
 auth_token = auth
 # fill in target csv file name in string below
 tickets_file = 'IMAP Diagnosis Spreadsheet Test'
