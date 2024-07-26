f13442360a5a75be51a3e9846047ef51cf466c0d
mastervalor <mouradmarzouk63@gmail.com>
2024-07-26 14:09:03 -0700

    Update assign_tickets.py

--- process/assign_tickets.py
+++ process/assign_tickets.py
@@ -1,3 +1,11 @@
 from logic.jira_logic.ticket_logic import Tickets
 
 tickets = Tickets()
+
+jql = ('project = "Corporate Engineering" and summary ~"needs to be rolled over to new standards" and summary !~ '
+       '"archived" and assignee is EMPTY')
+keys = tickets.get_ticket_keys_from_jql(jql)
+
+assignes = {"swaroop": 0, "srik": 0, "anand": 0}
+
+for key in keys:
