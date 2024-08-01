fa2d83a252f1fcb942580de5492550e3389442a2
mastervalor <mouradmarzouk63@gmail.com>
2024-08-01 16:40:23 -0700

    Update okta_user_logic.py

--- logic/okta_logic/okta_user_logic.py
+++ logic/okta_logic/okta_user_logic.py
@@ -1,6 +1,14 @@
 from calls.okta import Okta
 
-class Okta_users:
+
+class OktaUsers:
 
     def __init__(self):
         self.okta = Okta()
+
+    @classmethod
+    def get_user_manager(cls, email):
+        user_profile = cls.user_id(email)
+        manager = user_profile['profile']['manager']
+
+        return manager
