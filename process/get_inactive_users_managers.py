8bf33d9e18467343bde3af7a0e5ad0c529ccb37f
mastervalor <mouradmarzouk63@gmail.com>
2024-08-01 21:42:16 -0700

    Update get_inactive_users_managers.py

--- process/get_inactive_users_managers.py
+++ process/get_inactive_users_managers.py
@@ -6,3 +6,7 @@ tickets = Tickets()
 assignee_jql = 'project = "Enterprise CAPA" and assignee in inactiveUsers() and statusCategory != Done'
 
 reporter_jql = 'project = "Enterprise CAPA" and reporter in inactiveUsers() and statusCategory != Done'
+
+
+print(OktaUsers.get_user_manager('mourad.marzouk@getcruise.com'))
+print(OktaUsers.get_user_status('mourad.marzouk@getcruise.com'))
