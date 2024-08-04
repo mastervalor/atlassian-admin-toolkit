ff02f887efea160ba591de690d3cd80f52eb3a9a
mastervalor <mouradmarzouk63@gmail.com>
2024-08-03 18:19:47 -0700

    fixed title get and added catch for incomplete emails

--- logic/okta_logic/okta_user_logic.py
+++ logic/okta_logic/okta_user_logic.py
@@ -5,18 +5,24 @@ class OktaUsers:
 
     @classmethod
     def get_user_manager(cls, email):
+        if "@getcruise.com" not in email:
+            email = email + '@getcruise.com'
         user_profile = Okta.users_id(email)
         manager = user_profile['profile']['manager']
         return manager
 
     @classmethod
     def get_user_status(cls, email):
+        if "@getcruise.com" not in email:
+            email = email + '@getcruise.com'
         user_profile = Okta.users_id(email)
         user_status = user_profile['status']
         return user_status
 
     @classmethod
     def get_user_title(cls, email):
+        if "@getcruise.com" not in email:
+            email = email + '@getcruise.com'
         user_profile = Okta.users_id(email)
-        user_title = user_profile['title']
+        user_title = user_profile['profile']['title']
         return user_title
