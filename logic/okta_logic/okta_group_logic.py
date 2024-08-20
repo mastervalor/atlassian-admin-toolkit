9ffb3d1a3038430f1ccd57ba8140436f40e3ebe7
mastervalor <mouradmarzouk63@gmail.com>
2024-08-20 16:36:05 -0700

    Update okta_group_logic.py

--- logic/okta_logic/okta_group_logic.py
+++ logic/okta_logic/okta_group_logic.py
@@ -12,3 +12,14 @@ class OktaGroups:
         user2_groups = set(Okta.get_user_groups(user2_id))
 
         return user1_groups.difference(user2_groups)
+
+    @classmethod
+    def add_user_to_groups(cls, user, groups):
+        user_id = OktaUsers.get_user_id(user)
+        for group in groups:
+            group_id = Okta.get_group_id(group)
+            if "Failed to retrieve group ID" in group_id or "Group not found" in group_id:
+                print(f"Error: {group_id} for group: {group}. Skipping...")
+                continue
+            result = Okta.add_user_to_group(user_id, group_id)
+            print(f'User: {user} being added to group: {group} got result code: {result}')
