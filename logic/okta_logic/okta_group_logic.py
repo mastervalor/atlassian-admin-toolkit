bc6aaf63ff9977612f51898cf44cf19055ae9e33
mastervalor <mouradmarzouk63@gmail.com>
2024-08-20 14:07:37 -0700

    Update okta_group_logic.py

--- logic/okta_logic/okta_group_logic.py
+++ logic/okta_logic/okta_group_logic.py
@@ -6,4 +6,5 @@ class OktaGroups:
     def compare_users_groups(cls, user1, user2):
         user1_id = Okta.users_id(user1)
         user2_id = Okta.users_id(user2)
-        user1_groups =
+        user1_groups = Okta.get_user_groups(user1_id)
+        user2_groups = Okta.get_user_groups(user2_id)
