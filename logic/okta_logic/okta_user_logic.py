bd2757919627a63ec33c92098b1d57a45b7c4c7c
mastervalor <mouradmarzouk63@gmail.com>
2024-08-20 15:34:35 -0700

    Update okta_user_logic.py

--- logic/okta_logic/okta_user_logic.py
+++ logic/okta_logic/okta_user_logic.py
@@ -7,7 +7,7 @@ class OktaUsers:
     def get_user_manager(cls, email):
         if "@getcruise.com" not in email:
             email = email + '@getcruise.com'
-        user_profile = Okta.users_id(email)
+        user_profile = Okta.users_profile(email)
         try:
             manager = user_profile['profile']['manager']
             return manager
@@ -19,7 +19,7 @@ class OktaUsers:
     def get_user_status(cls, email):
         if "@getcruise.com" not in email:
             email = email + '@getcruise.com'
-        user_profile = Okta.users_id(email)
+        user_profile = Okta.users_profile(email)
         user_status = user_profile['status']
         return user_status
 
@@ -27,7 +27,7 @@ class OktaUsers:
     def get_user_title(cls, email):
         if "@getcruise.com" not in email:
             email = email + '@getcruise.com'
-        user_profile = Okta.users_id(email)
+        user_profile = Okta.users_profile(email)
         user_title = user_profile['profile']['title']
         return user_title
 
