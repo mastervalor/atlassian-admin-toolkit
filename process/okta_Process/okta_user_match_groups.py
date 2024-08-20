57dd8bab1b061f54b527d75278a3c1169e2e74fd
mastervalor <mouradmarzouk63@gmail.com>
2024-08-20 14:32:51 -0700

    Update okta_user_match_groups.py

--- process/okta_Process/okta_user_match_groups.py
+++ process/okta_Process/okta_user_match_groups.py
@@ -1,1 +1,6 @@
 from logic.okta_logic.okta_group_logic import OktaGroups
+
+user1 = 'satchidanand.challapalli@getcruise.com'
+user2 = 'sumaiah.syed@getcruise.com'
+
+compared_groups = OktaGroups.compare_users_groups()
