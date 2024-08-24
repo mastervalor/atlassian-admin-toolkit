d2cad70144346b32bd13a9b2e396c580135f733a
mastervalor <mouradmarzouk63@gmail.com>
2024-08-23 18:52:23 -0700

    Update delete_users_by_string.py

--- process/jira_users_process/delete_users_by_string.py
+++ process/jira_users_process/delete_users_by_string.py
@@ -1,5 +1,13 @@
 from logic.jira_logic.user_logic import Users
+from calls.jira import Jira
+import json
 
-user = Users(is_staging=True)
+# user = Users(is_staging=True)
+#
+# user.get_usernames_by_search_string('')
 
-user.get_usernames_by_search_string('es.cloudfactory.com')
+jira = Jira(is_staging=True)
+
+user = jira.find_users_by_string('mourad.marzouk', 50, 0)
+
+print(user[0]['emailAddress'])
