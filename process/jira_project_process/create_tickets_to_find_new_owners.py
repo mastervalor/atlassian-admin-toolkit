c504b72225e0bd1625154dcde8d3a211d93767ac
mastervalor <mouradmarzouk63@gmail.com>
2024-08-28 21:14:23 -0700

    Update create_tickets_to_find_new_owners.py

--- process/jira_project_process/create_tickets_to_find_new_owners.py
+++ process/jira_project_process/create_tickets_to_find_new_owners.py
@@ -1,4 +1,5 @@
 from logic.jira_logic.project_logic import Projects
+from logic.okta_logic.okta_user_logic import OktaUsers
 
 
 project = Projects()
@@ -6,3 +7,42 @@ projects = project.get_project_owners_and_status()
 
 for project in projects:
     if not project['Active']:
+        manager = OktaUsers.get_manager_info()
+        payload = {
+            'fields': {
+                'project': {
+                    'key': 'ITAPP',
+                },
+                'summary': f"project: {project['project']}, Key: {project['Key']} no longer has and active owner.",
+                "issuetype": {
+                    "id": "3"
+                },
+                "reporter": {
+                    "name": 'mourad.marzouk'
+                },
+                "customfield_18672": {
+                    "value": "Strategic Work"
+                },
+                "customfield_28001": {
+                    'value': "Jira"
+                },
+                "description": f"project: {project['project']}, Key: {project['Key']} no longer has and active owner."
+                               f"Last owner is {project['Name']}, their lister manager is "
+            },
+            "update": {
+                "issuelinks": [
+                    {
+                        "add": {
+                            "type": {
+                                "name": "Problem/Incident",
+                                "inward": "is caused by",
+                                "outward": "causes"
+                            },
+                            "outwardIssue": {
+                                "key": "ITAPP-7856"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
