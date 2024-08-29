1841e3ce72fad1620cb996d54d590cfbfb36566b
mastervalor <mouradmarzouk63@gmail.com>
2024-08-28 21:33:53 -0700

    Update create_tickets_to_find_new_owners.py

--- process/jira_project_process/create_tickets_to_find_new_owners.py
+++ process/jira_project_process/create_tickets_to_find_new_owners.py
@@ -1,49 +1,22 @@
 from logic.jira_logic.project_logic import Projects
 from logic.okta_logic.okta_user_logic import OktaUsers
+from logic.jira_logic.ticket_logic import Tickets
 
-
+tickets = Tickets()
 project = Projects()
 projects = project.get_project_owners_and_status()
 
 for project in projects:
     if not project['Active']:
         manager, manager_title, manager_status  = OktaUsers.get_manager_info(project['Username'])
-        payload = {
-            'fields': {
-                'project': {
-                    'key': 'ITAPP',
-                },
-                'summary': f"project: {project['project']}, Key: {project['Key']} no longer has and active owner.",
-                "issuetype": {
-                    "id": "3"
-                },
-                "reporter": {
-                    "name": 'mourad.marzouk'
-                },
-                "customfield_18672": {
-                    "value": "Strategic Work"
-                },
-                "customfield_28001": {
-                    'value': "Jira"
-                },
-                "description": f"project: {project['project']}, Key: {project['Key']} no longer has and active owner."
-                               f"Last owner is {project['Name']} and we need to find a replacement."
-                               f"Their listed manager is {manager}, Title{manager_title}, Status{manager_status}"
-            },
-            "update": {
-                "issuelinks": [
-                    {
-                        "add": {
-                            "type": {
-                                "name": "Problem/Incident",
-                                "inward": "is caused by",
-                                "outward": "causes"
-                            },
-                            "outwardIssue": {
-                                "key": "ITAPP-7856"
-                            }
-                        }
-                    }
-                ]
-            }
+        ticket_info = {
+            'summary': f"project: {project['project']}, Key: {project['Key']} no longer has and active owner.",
+            'assignee': 'mourad.marzouk',
+            'description': f"project: {project['project']}, Key: {project['Key']} no longer has and active owner."
+                           f"Last owner is {project['Name']} and we need to find a replacement."
+                           f"Their listed manager is {manager}, Title{manager_title}, Status{manager_status}",
+            'parent ticket': "CORPENG-12665"
         }
+
+        tickets.build_ticket_payload(ticket_info)
+        
