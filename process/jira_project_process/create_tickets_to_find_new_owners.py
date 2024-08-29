2ecbf983bf30fccf59f782a39b9c38e8b5b60620
mastervalor <mouradmarzouk63@gmail.com>
2024-08-28 21:44:00 -0700

    Update create_tickets_to_find_new_owners.py

--- process/jira_project_process/create_tickets_to_find_new_owners.py
+++ process/jira_project_process/create_tickets_to_find_new_owners.py
@@ -8,15 +8,14 @@ projects = project.get_project_owners_and_status()
 
 for project in projects:
     if not project['Active']:
-        manager, manager_title, manager_status  = OktaUsers.get_manager_info(project['Username'])
+        manager, manager_title, manager_status = OktaUsers.get_manager_info(project['Username'])
         ticket_info = {
-            'summary': f"project: {project['project']}, Key: {project['Key']} no longer has and active owner.",
+            'summary': f"project: {project['Project']}, Key: {project['Key']} no longer has and active owner.",
             'assignee': 'mourad.marzouk',
-            'description': f"project: {project['project']}, Key: {project['Key']} no longer has and active owner."
+            'description': f"project: {project['Project']}, Key: {project['Key']} no longer has and active owner."
                            f"Last owner is {project['Name']} and we need to find a replacement."
                            f"Their listed manager is {manager}, Title{manager_title}, Status{manager_status}",
             'parent ticket': "CORPENG-12665"
         }
 
         tickets.build_ticket_payload(ticket_info)
-        
