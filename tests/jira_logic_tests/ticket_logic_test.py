0b214546aa1c83bb14bcae568a7b9e7826e9a69e
mastervalor <mouradmarzouk63@gmail.com>
2024-08-29 21:59:06 -0700

    Update ticket_logic_test.py

--- tests/jira_logic_tests/ticket_logic_test.py
+++ tests/jira_logic_tests/ticket_logic_test.py
@@ -1,5 +1,8 @@
 from logic.jira_logic.ticket_logic import Tickets
 from dataformating.json_formating import JSONFormating
+import unittest
+from unittest.mock import MagicMock, patch
+
 
 tickets = Tickets()
 formating = JSONFormating()
