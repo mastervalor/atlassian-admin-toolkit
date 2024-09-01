1e890575ff151dce382e10797cc6cd02add12581
mastervalor <mouradmarzouk63@gmail.com>
2024-08-31 23:23:55 -0700

    Update group_users_logic_test.py

---
+++ tests/jira_logic_tests/group_users_logic_test.py
@@ -0,0 +1,4 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from logic.jira_logic.groups_users_logic import GroupsUsers  # Replace with your actual import path
+from calls.jira import Jira
