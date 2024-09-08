d7fa3635b71316065fbae664625658cd1b8c8d74
mastervalor <mouradmarzouk63@gmail.com>
2024-09-08 16:03:38 -0700

    Update edit_ticket.py

--- process/jira_ticket_process/edit_ticket.py
+++ process/jira_ticket_process/edit_ticket.py
@@ -4,3 +4,10 @@ from logic.os_logic.os_logic import OSLogic
 tickets = Tickets()
 os_logic = OSLogic(open_file='Design Intent Data')
 
+file = os_logic.read_file()
+
+for ticket in file:
+    summary = ticket['Summary']
+    jql = f'summary ~"{summary}" and project = "Behavior Requirements & Monitoring"'
+    key = tickets.get_ticket_keys_from_jql(jql)[0]
+    print(key)
