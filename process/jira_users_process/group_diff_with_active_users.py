38e6c0d52ddce1039149a9a37fe3b0ff8952fe73
mastervalor <mouradmarzouk63@gmail.com>
2024-09-09 23:29:17 -0700

    Update group_diff_with_active_users.py

--- process/jira_users_process/group_diff_with_active_users.py
+++ process/jira_users_process/group_diff_with_active_users.py
@@ -1,25 +1,42 @@
-import json
 import os
 import csv
 from logic.os_logic.os_logic import OSLogic
 from logic.jira_logic.groups_users_logic import GroupsUsers
 from logic.okta_logic.okta_user_logic import OktaUsers
 
+# Initialize necessary classes
 group_users = GroupsUsers()
-os_logic = OSLogic()
+okta_users = OktaUsers()
+new_file = 'not in app-jira with status'
+open_file = 'not in app-jira'
 
-newFile = 'not in app-jira with status'
-openFile = 'not in app-jira'
+# Set up the OSLogic instance for reading and writing
+os_logic_read = OSLogic(open_file=open_file)
+os_logic_write = OSLogic(write_file=new_file, columns=['Username', 'access group', 'status', 'in okta'])
 
-with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
-    writer = csv.writer(new_csv)
-    writer.writerow(['Username', 'access group', 'status', 'in okta'])
-    with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), openFile), mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for i in csv_reader:
-            status = group_users.get_user_status(i['Username'])
-            okta = OktaUsers.get_user_id(f"{i['Username']}@getcruise.com")
-            if okta:
-                okta = okta['status']
-            writer.writerow([i['Username'], i['access group'], status, okta])
-            print(f"User {i['Username']} is active: {status} and oka: {okta}")
+# Read the data from the existing CSV file
+csv_data = os_logic_read.read_file()
+
+# Prepare data to be written to the new file
+data_to_write = []
+
+for row in csv_data:
+    # Get the status of the user from GroupsUsers
+    status = group_users.get_user_status(row['Username'])
+
+    # Get the user ID from OktaUsers
+    okta = okta_users.get_user_id(f"{row['Username']}@getcruise.com")
+    okta_status = okta['status'] if okta else None
+
+    # Append the data to the list to write later
+    data_to_write.append({
+        'Username': row['Username'],
+        'access group': row['access group'],
+        'status': status,
+        'in okta': okta_status
+    })
+
+    print(f"User {row['Username']} is active: {status} and okta: {okta_status}")
+
+# Write the data to the new file
+os_logic_write.write_to_file(data_to_write)
