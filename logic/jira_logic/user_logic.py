34bf14d6614e80a084edce3aacfd56c46ac6fdac
mastervalor <mouradmarzouk63@gmail.com>
2024-09-10 23:33:01 -0700

    Update user_logic.py

--- logic/jira_logic/user_logic.py
+++ logic/jira_logic/user_logic.py
@@ -4,10 +4,10 @@ from dataformating.json_formating import JSONFormating
 
 class Users:
     def __init__(self, is_staging=False):
-        self.jira = UserJiraCalls(is_staging=True) if is_staging else UserJiraCalls()
+        self.jira_users = UserJiraCalls(is_staging=True) if is_staging else UserJiraCalls()
 
     def user_groups(self, user):
-        groups = self.jira.get_user(user, '?expand=groups')
+        groups = self.jira_users.get_user(user, '?expand=groups')
         user_groups = []
 
         for group in groups['groups']['items']:
@@ -16,14 +16,14 @@ class Users:
         return user_groups
 
     def get_user_status(self, user):
-        user_profile = self.jira.get_user(user)
+        user_profile = self.jira_users.get_user(user)
         if user_profile['active']:
             return 'Active'
         else:
             return 'Inactive'
 
     def get_user_applications(self, user):
-        user_profile = self.jira.get_user(user, '?expand=applicationRoles')
+        user_profile = self.jira_users.get_user(user, '?expand=applicationRoles')
         roles = []
         for role in user_profile['applicationRoles']['items']:
             roles.append(role['name'])
@@ -32,7 +32,7 @@ class Users:
 
     def delete_list_of_users(self, users):
         for user in users:
-            response = self.jira.delete_user(user)
+            response = self.jira_users.delete_user(user)
             print(response)
 
     def get_usernames_by_domain_string(self, users_search_string):
@@ -41,9 +41,8 @@ class Users:
         total = 51
         users_found = []
         while total >= max_results:
-            users = self.jira.get_group(f'?includeInactiveUsers=false&startAt={start_at}&maxResults={max_results}'
-                                        f'&includeInactive=True',
-                                        'app-jira')
+            users = self.jira_users.get_group(f'?includeInactiveUsers=false&startAt={start_at}'
+                                              f'&maxResults={max_results}&includeInactive=True', 'app-jira')
             for user in users['values']:
                 if users_search_string in user['emailAddress']:
                     users_found.append(user['name'])
