4e0a12512085a9b51746f2f6b4aac9d368335206
mastervalor <mouradmarzouk63@gmail.com>
2024-09-11 22:56:02 -0700

    Update jira_api_group_calls.py

--- calls/jira_api_calls/jira_api_group_calls.py
+++ calls/jira_api_calls/jira_api_group_calls.py
@@ -2,33 +2,12 @@ import requests
 from auth import auth, staging_auth
 import json
 from config import jira, jira_staging
-from calls.jira_api_calls.jira_api_user_calls import UserJiraCalls
 
 
 class GroupJiraCalls:
     def __init__(self, is_staging=False):
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
-        self.jira_users = UserJiraCalls()
-
-    def group_members(self, group):
-        start_at = 0
-        max_results = 50
-        total = 51
-        members_list = []
-
-        while total >= max_results:
-            members = self.jira_users.get_group(f'?includeInactiveUsers=false&startAt={start_at}'
-                                                f'&maxResults={max_results}', group)
-
-            for member in members['values']:
-                members_list.append(member['name'])
-
-            total = members['total']
-            start_at += 50
-            max_results += 50
-            # print(startAt, maxResults)
-        return members_list
 
     def remove_group_member(self, group, user):
         url = self.jira + 'group/user'
@@ -44,5 +23,24 @@ class GroupJiraCalls:
             params=query,
             auth=self.token
         ).text
+
+        return response
+
+    def get_group(self, pref, group):
+        url = self.jira + 'group/member' + pref
+
+        headers = {
+            "Accept": "application/json"
+        }
+        query = {
+            'groupname': group
+        }
+        response = json.loads(requests.request(
+            "GET",
+            url,
+            headers=headers,
+            params=query,
+            auth=self.token
+        ).text)
 
         return response
