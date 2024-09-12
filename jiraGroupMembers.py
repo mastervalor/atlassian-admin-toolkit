9951da0acd1edc84c0bd88b393ff1f1b6acdabff
mastervalor <mouradmarzouk63@gmail.com>
2024-09-11 22:55:57 -0700

    Update jiraGroupMembers.py

--- jiraGroupMembers.py
+++ jiraGroupMembers.py
@@ -1,9 +1,8 @@
-import json
+from logic.jira_logic.groups_users_logic import GroupsUsers
 import csv
 import os
-from call import Jira
 
-jira = Jira()
+jira = GroupsUsers()
 newFile = "app-jira members"
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
