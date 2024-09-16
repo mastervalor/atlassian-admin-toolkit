9224d83b0696ecb32f79130ed788b43e84f6f141
mastervalor <mouradmarzouk63@gmail.com>
2024-09-15 17:33:59 -0700

    Update projects_not_on_standards.py

--- process/jira_project_process/projects_not_on_standards.py
+++ process/jira_project_process/projects_not_on_standards.py
@@ -2,6 +2,19 @@ from logic.os_logic.os_logic import OSLogic
 
 FINAL_LIST_OF_PROJECTS = []
 
+
+def add_to_the_list(file):
+    for i in file:
+        row = i['Project Key or Project Link (I.E. "CORPENG" for Corporate Engineering Project)']
+        if ',' in row:
+            projects = row.split(', ')
+            for project in projects:
+                if project not in FINAL_LIST_OF_PROJECTS:
+                    FINAL_LIST_OF_PROJECTS.append(project)
+        else:
+            FINAL_LIST_OF_PROJECTS.append(row)
+
+
 jsm = OSLogic(open_file='JSM')
 extras = OSLogic(open_file='extras')
 software = OSLogic(open_file='Software standardization')
@@ -10,17 +23,6 @@ jsm_file = jsm.read_file()
 extra_file = extras.read_file()
 software_file = software.read_file()
 
-final_lis_of_projects = []
+add_to_the_list(extra_file)
 
-for i in extra_file:
-    row = i['Project Key or Project Link (I.E. "CORPENG" for Corporate Engineering Project)']
-    if ',' in row:
-        projects = row.split(', ')
-        for project in projects:
-            if project not in final_lis_of_projects:
-                final_lis_of_projects.append(project)
-    else:
-        final_lis_of_projects.append(row)
-
-
-print(final_lis_of_projects)
+print(FINAL_LIST_OF_PROJECTS)
