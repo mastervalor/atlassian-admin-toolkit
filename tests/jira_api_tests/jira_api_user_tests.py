364bc32577cd3a0a3d4198175d744c7462cb7975
mastervalor <mouradmarzouk63@gmail.com>
2024-09-15 22:44:01 -0700

    Update jira_api_user_tests.py

--- tests/jira_api_tests/jira_api_user_tests.py
+++ tests/jira_api_tests/jira_api_user_tests.py
@@ -15,3 +15,7 @@ class TestUserJiraCalls(unittest.TestCase):
         user = 'test.user'
         response = self.jira_user_calls.delete_user(user)
         self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
