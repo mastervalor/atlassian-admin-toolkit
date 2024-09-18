1c0cab16efeba30dc9f2c31788ad7d8cedcf7251
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 16:49:15 -0700

    Create dashboard_logic.py

---
+++ logic/jira_logic/dashboard_logic.py
@@ -0,0 +1,5 @@
+from calls.jira_api_calls.jira_api_dashboards import DashboardsJiraCalls
+
+class Dashboards:
+    def __init__(self, is_staging=False):
+        self.dashboards = DashboardsJiraCalls()
