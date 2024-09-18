ea90a5270a768b1954cc3482fb1a0837182e4406
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 16:38:58 -0700

    Update jira_api_dashboards.py

--- calls/jira_api_calls/jira_api_dashboards.py
+++ calls/jira_api_calls/jira_api_dashboards.py
@@ -8,3 +8,14 @@ class DashboardsJiraCalls:
     def __init__(self, is_staging=False):
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
+
+    def delete_dashboard(self, dashboard_id):
+        url = self.jira + f'dashboard/{dashboard_id}'
+
+        response = requests.request(
+            "DELETE",
+            url,
+            auth=self.token
+        )
+
+        return response
