2462828060feb2196dd71cb70a0960b97657fc48
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 22:20:53 -0700

    Update jira_api_dashboards_calls_test.py

--- tests/jira_api_tests/jira_api_dashboards_calls_test.py
+++ tests/jira_api_tests/jira_api_dashboards_calls_test.py
@@ -10,3 +10,17 @@ class TestDashBoardJiraCalls(unittest.TestCase):
         dashboard_id = '24918'
         response = self.jira_dashboard_calls.delete_dashboard(dashboard_id)
         self.assertEqual(response.status_code, 200)
+
+    def test_get_all_dashboards(self):
+        pref = ''
+        response = self.jira_dashboard_calls.get_all_dashboards(pref)
+        self.assertIn("total", response)
+
+    def test_get_all_boards(self):
+        response = self.jira_dashboard_calls.get_all_boards()
+        self.assertIn("total", response)
+
+    def test_delete_board(self):
+        board_id = '1428'
+        response = self.jira_dashboard_calls.delete_board(board_id)
+        self.assertEqual(response.status_code, 200)
