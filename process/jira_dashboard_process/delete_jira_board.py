a470c88dfe859dbef1afb37587ca2f071575ba8f
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 21:38:18 -0700

    Update delete_jira_board.py

--- process/jira_dashboard_process/delete_jira_board.py
+++ process/jira_dashboard_process/delete_jira_board.py
@@ -1,13 +1,7 @@
-from pprint import pprint
-
-from calls.jira_api_calls.jira_api_dashboards import DashboardsJiraCalls
 from logic.jira_logic.dashboard_logic import Dashboards
 
-# dashboards = Dashboards()
-#
-# pprint(dashboards.get_board_ids('C5 EV Builds'))
-boards = [1224, 1225, 1226]
+dashboards = Dashboards()
 
-dashboards = DashboardsJiraCalls()
+boards = dashboards.get_board_ids('C5 EV Builds - Sunny')
 
-pprint(dashboards.delete_board(str(1224)))
+print(dashboards.delete_boards(boards))
