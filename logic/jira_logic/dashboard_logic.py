6cc4002bab2a491cace567611adbb62cbf32e8a8
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 18:44:17 -0700

    Update dashboard_logic.py

--- logic/jira_logic/dashboard_logic.py
+++ logic/jira_logic/dashboard_logic.py
@@ -4,3 +4,21 @@ from calls.jira_api_calls.jira_api_dashboards import DashboardsJiraCalls
 class Dashboards:
     def __init__(self, is_staging=False):
         self.dashboards = DashboardsJiraCalls()
+
+    def get_dashboard_id(self, name):
+        start_at = 0
+        max_results = 1000
+        total = 1001
+        ids = []
+        dashboards = self.dashboards.get_all_dashboards(f'?startAt={start_at}&maxResults={max_results}')
+
+        while total >= max_results:
+            for dashboard in dashboards['dashboards']:
+                if dashboard['name'].lower() == name.lower():
+                    ids.append(dashboard['id'])
+            else:
+                total = dashboards['total']
+                start_at += 1000
+                max_results += 1000
+
+        return ids
