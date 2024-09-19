6354b020a73d82f8cbe72dd76f8c9f131df242ef
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 19:27:42 -0700

    Update dashboard_logic.py

--- logic/jira_logic/dashboard_logic.py
+++ logic/jira_logic/dashboard_logic.py
@@ -25,19 +25,17 @@ class Dashboards:
 
     def get_board_ids(self, name):
         start_at = 0
-        max_results = 1000
-        total = 1001
+        total = 51
         ids = []
 
-        while total >= max_results:
-            boards = self.dashboards.get_all_boards(start_at, max_results)
-            for dashboard in boards['dashboards']:
-                if dashboard['name'].lower() == name.lower():
-                    ids.append(dashboard['id'])
+        while total >= start_at:
+            boards = self.dashboards.get_all_boards(start_at)
+            for board in boards['values']:
+                if board['name'].lower() == name.lower():
+                    ids.append(board['id'])
             else:
                 total = boards['total']
-                start_at += 1000
-                max_results += 1000
+                start_at += 50
 
         return ids
 
