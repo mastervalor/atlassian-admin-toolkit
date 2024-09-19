549695feb24270d5e1b96f24cabf1283ef7da0c8
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 18:58:56 -0700

    Update delete_jira_dashboard.py

--- process/jira_dashboard_process/delete_jira_dashboard.py
+++ process/jira_dashboard_process/delete_jira_dashboard.py
@@ -3,6 +3,6 @@ from pprint import pprint
 from calls.jira_api_calls.jira_api_dashboards import DashboardsJiraCalls
 from logic.jira_logic.dashboard_logic import Dashboards
 
-dashboards = Dashboards()
+dashboards = DashboardsJiraCalls()
 
-pprint(dashboards.get_dashboard_id('10X Labeling Quality'))
+pprint(dashboards.get_all_boards())
