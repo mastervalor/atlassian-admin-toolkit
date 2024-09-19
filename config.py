914650f59f1bc9b47ff47d832338420ab81e6c51
mastervalor <mouradmarzouk63@gmail.com>
2024-09-18 18:45:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ jira = "https://jira.robot.car/rest/api/2/"
 jira_staging = "https://jira-dev.robot.car/rest/api/2/"
 okta = 'https://cruise.okta.com/api/v1/'
 confluence_staging = 'https://wiki.stage.robot.car/rest/api/'
+jira_agile = "https://jira.robot.car/rest/agile/1.0/"
