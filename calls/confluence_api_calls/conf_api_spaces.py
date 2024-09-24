a47c93228e8ace2f0f197167d00c25ded8f31605
mastervalor <mouradmarzouk63@gmail.com>
2024-09-24 14:49:57 -0700

    Update conf_api_spaces.py

--- calls/confluence_api_calls/conf_api_spaces.py
+++ calls/confluence_api_calls/conf_api_spaces.py
@@ -1,4 +1,27 @@
 import requests
-from auth import conf_token, staging_conf_token
+from auth import conf_cloud_dev_token
 import json
-from config import confluence, confluence_staging, conf_base
+from config import conf_cloud_v1, conf_cloud_v2, conf_cloud_v1_dev, conf_cloud_v2_dev
+
+
+class ConfluenceSpaceCalls:
+    def __init__(self, is_staging=False):
+        self.cloud_v1 = conf_cloud_v1_dev if is_staging else conf_cloud_v1
+        self.cloud_v2 = conf_cloud_v2_dev if is_staging else conf_cloud_v2
+        self.token = conf_cloud_dev_token
+
+    def get_spaces(self):
+        url = self.cloud_v2
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        response = json.loads(requests.request(
+            "GET",
+            url,
+            headers=headers,
+            auth=self.token
+        ).text)
+
+        return response
