791d97702ebedbd72b5bfdf74229177c23d56899
mastervalor <mouradmarzouk63@gmail.com>
2024-09-24 19:10:11 -0700

    Update space_logic.py

--- logic/confluence_logic/space_logic.py
+++ logic/confluence_logic/space_logic.py
@@ -5,8 +5,11 @@ class Spaces:
     def __init__(self, is_staging=False):
         self.conf_spaces = ConfluenceSpaceCalls(is_staging=True) if is_staging else ConfluenceSpaceCalls()
 
-    def get_project_ids(self, spaces_list):
-        all_spaces = self.conf_spaces.get_spaces()
+    def get_all_spaces(self):
+        all_spaces = self.conf_spaces.get_spaces()['results']
 
-        for spaces in all_spaces['results']:
+    def get_space_ids(self, spaces_list):
+        all_spaces = self.conf_spaces.get_spaces()['results']
+        print(len(all_spaces))
+        for spaces in all_spaces:
             print(spaces)
