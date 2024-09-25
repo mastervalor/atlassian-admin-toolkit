63a7b1a978bf0f7d0fbbd0c80b1548b966ed810c
mastervalor <mouradmarzouk63@gmail.com>
2024-09-24 19:15:33 -0700

    Update space_logic.py

--- logic/confluence_logic/space_logic.py
+++ logic/confluence_logic/space_logic.py
@@ -6,7 +6,23 @@ class Spaces:
         self.conf_spaces = ConfluenceSpaceCalls(is_staging=True) if is_staging else ConfluenceSpaceCalls()
 
     def get_all_spaces(self):
-        all_spaces = self.conf_spaces.get_spaces()['results']
+        all_spaces = []
+        cursor = None
+        while True:
+            # Fetch a single page of spaces
+            response_data = self.conf_spaces.get_spaces(cursor=cursor)
+
+            if not response_data:
+                break
+
+            all_spaces.extend(response_data.get('results', []))
+
+            if "_links" in response_data and "next" in response_data["_links"]:
+                cursor = response_data["_links"]["next"]  # Update cursor for the next batch
+            else:
+                break
+
+        return all_spaces
 
     def get_space_ids(self, spaces_list):
         all_spaces = self.conf_spaces.get_spaces()['results']
