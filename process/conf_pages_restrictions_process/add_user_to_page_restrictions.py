b17e3e899dc2be6daa90181b9afc79c806151b69
mastervalor <mouradmarzouk63@gmail.com>
2024-09-24 19:24:25 -0700

    Update add_user_to_page_restrictions.py

--- process/conf_pages_restrictions_process/add_user_to_page_restrictions.py
+++ process/conf_pages_restrictions_process/add_user_to_page_restrictions.py
@@ -3,7 +3,7 @@ from logic.confluence_logic.space_logic import Spaces
 # get pages Ids from a list of names
 spaces = Spaces(is_staging=True)
 spaces_list =[]
-spaces.get_project_ids(spaces_list)
+spaces.get_space_ids(spaces_list)
 # get all restricted pages in a space and their IDs
 # add user to all those restricted pages with edit
 
