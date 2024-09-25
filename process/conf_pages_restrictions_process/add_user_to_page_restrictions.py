81709aaf42cea08fe5a2f69ffda43ecae4230bce
mastervalor <mouradmarzouk63@gmail.com>
2024-09-24 22:35:42 -0700

    Update add_user_to_page_restrictions.py

--- process/conf_pages_restrictions_process/add_user_to_page_restrictions.py
+++ process/conf_pages_restrictions_process/add_user_to_page_restrictions.py
@@ -1,9 +1,17 @@
 from logic.confluence_logic.space_logic import Spaces
+from logic.os_logic.os_logic import OSLogic
 
 # get pages Ids from a list of names
 spaces = Spaces(is_staging=True)
-spaces_list =[]
-spaces.get_space_ids(spaces_list)
-# get all restricted pages in a space and their IDs
-# add user to all those restricted pages with edit
+os_logic = OSLogic(open_file='LucidChart')
+lucid_chart = os_logic.read_file()
+spaces_list = []
 
+for space in lucid_chart:
+    spaces_list.append(space['Space Name'])
+
+print(spaces_list)
+print(spaces.get_space_ids(spaces_list))
+# get all restricted pages in a space and their IDs
+
+# add user to all those restricted pages with edit
