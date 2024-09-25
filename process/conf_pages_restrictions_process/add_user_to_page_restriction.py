b802e79a6d85091eea479954c62f2ca5e043c978
mastervalor <mouradmarzouk63@gmail.com>
2024-09-25 14:46:38 -0700

    Update add_user_to_page_restriction.py

--- process/conf_pages_restrictions_process/add_user_to_page_restriction.py
+++ process/conf_pages_restrictions_process/add_user_to_page_restriction.py
@@ -1,5 +1,14 @@
 from logic.confluence_logic.space_logic import Spaces
+from calls.confluence_api_calls.conf_api_spaces import ConfluenceSpaceCalls
+from dataformating.json_formating import JSONFormating
+
 
 spaces = Spaces(is_staging=True)
+conf_spaces = ConfluenceSpaceCalls(is_staging=True)
 
 Page_id = '96207758'
+user_id = '557058:9ab63286-11ed-497d-8147-88b76e6c8a56'
+
+JSONFormating.pretty_json(conf_spaces.fetch_restrictions_for_page(Page_id))
+
+
