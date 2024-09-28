0527aa13a538a9194074ff1efe2f5352c66405ff
mastervalor <mouradmarzouk63@gmail.com>
2024-09-28 10:30:41 -0700

    Update get_spaces_restricted_pages.py

--- process/conf_pages_restrictions_process/get_spaces_restricted_pages.py
+++ process/conf_pages_restrictions_process/get_spaces_restricted_pages.py
@@ -2,7 +2,7 @@ from logic.confluence_logic.space_logic import Spaces
 from logic.os_logic.os_logic import OSLogic
 
 # get pages Ids from a list of names
-spaces = Spaces(is_staging=True)
+spaces = Spaces()
 os_logic = OSLogic(open_file='LucidChart', write_file='lucid chart page ids', columns=['Page ID'])
 lucid_chart = os_logic.read_file()
 spaces_list = []
@@ -13,12 +13,12 @@ for space in lucid_chart:
 
 space_ids = spaces.get_space_ids(spaces_list)
 # get all restricted pages in a space and their IDs
-for space_id in space_ids.values():
-    restricted_pages = spaces.get_restricted_pages_in_space(space_id)
+for key,value in space_ids.items():
+    restricted_pages = spaces.get_restricted_pages_in_space(value)
     page_ids.extend(restricted_pages)  # Add these pages to the main page_ids list
 
     # Print progress to track the process
-    print(f"Space: {space_id} had restricted pages: {len(restricted_pages)}")
+    print(f"Space: {key}, ID: {value} had restricted pages: {len(restricted_pages)}")
 
 data_rows = [{'Page ID': page_id} for page_id in page_ids]
 os_logic.write_to_file(data_rows)
