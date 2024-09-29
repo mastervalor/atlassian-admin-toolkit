42a28f4b06ae551f036d7b01c997ab4d264ed971
mastervalor <mouradmarzouk63@gmail.com>
2024-09-28 22:34:08 -0700

    Update add_user_to_page_restriction.py

--- process/conf_pages_restrictions_process/add_user_to_page_restriction.py
+++ process/conf_pages_restrictions_process/add_user_to_page_restriction.py
@@ -5,21 +5,20 @@ from logic.os_logic.os_logic import OSLogic
 from dataformating.json_formating import JSONFormating
 
 os_logic = OSLogic(open_file='lucid chart page ids')
-spaces = Spaces(is_staging=True)
-conf_spaces = ConfluenceSpaceCalls(is_staging=True)
-conf_pages = ConfluencePageCalls(is_staging=True)
+restricted_pages = os_logic.read_file()
+conf_pages = ConfluencePageCalls()
 
-page_id = '64794020'
+
 user_id = '557058:9ab63286-11ed-497d-8147-88b76e6c8a56'
 my_id = '63c996ae6178fcc941d947ad'
 
-# JSONFormating.pretty_json(conf_spaces.fetch_restrictions_for_page(Page_id))
-self_response = conf_pages.add_user_to_page_restriction(page_id, 'update', my_id)
-print(self_response.status_code)
-print(self_response.text)
+for page_id in restricted_pages:
+    self_response = conf_pages.add_user_to_page_restriction(page_id['Page ID'], 'update', my_id)
+    print(f'page is: {page_id}: has come back with status code:  {self_response.status_code}')
+    print(self_response.text)
 
-response = conf_pages.add_user_to_page_restriction(page_id, 'update', user_id)
+    response = conf_pages.add_user_to_page_restriction(page_id['Page ID'], 'update', user_id)
 
-print(response.status_code)
-print(response.text)
+    print(f'page is: {page_id}: has come back with status code:  {response.status_code}')
+    print(response.text)
 
