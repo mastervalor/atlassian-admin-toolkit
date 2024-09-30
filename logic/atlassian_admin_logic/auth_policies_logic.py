a0626ddcaebd9045cabb613305a4e2b7db2faf80
mastervalor <mouradmarzouk63@gmail.com>
2024-09-30 10:06:37 -0700

    Update auth_policies_logic.py

--- logic/atlassian_admin_logic/auth_policies_logic.py
+++ logic/atlassian_admin_logic/auth_policies_logic.py
@@ -5,6 +5,15 @@ class AuthPolicies:
     def __init__(self):
         self.auth_policies = AtlassianAuthPolicies()
 
+    def get_status_of_task(self,task_id):
+        response = self.auth_policies
+
     def add_users_to_auth_policies(self, users, policy_id):
+        user_responses = []
         for user in users:
-            self.auth_policies.add_user_to_policy(user, policy_id)
+            response = self.auth_policies.add_user_to_policy(user, policy_id)
+            user_responses.append({
+                'user': user,
+                'task_id': response['taskId'],
+                'task_link': response['taskLink']
+            })
