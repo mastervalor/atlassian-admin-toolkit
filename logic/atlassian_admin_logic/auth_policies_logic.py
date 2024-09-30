bdb71eb5189d84a5265518d813f50855e35c6180
mastervalor <mouradmarzouk63@gmail.com>
2024-09-30 09:30:32 -0700

    Update auth_policies_logic.py

--- logic/atlassian_admin_logic/auth_policies_logic.py
+++ logic/atlassian_admin_logic/auth_policies_logic.py
@@ -1,7 +1,10 @@
 from calls.atlassian_admin_api_calls.auth_policies_api import AtlassianAuthPolicies
 
+
 class AuthPolicies:
     def __init__(self):
         self.auth_policies = AtlassianAuthPolicies()
 
     def add_users_to_auth_policies(self, users, policy_id):
+        for user in users:
+            self.auth_policies.add_user_to_policy(user, policy_id)
