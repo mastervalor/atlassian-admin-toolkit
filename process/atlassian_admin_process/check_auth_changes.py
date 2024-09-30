37498faa509498b72d89edb1c80cf0a611590018
mastervalor <mouradmarzouk63@gmail.com>
2024-09-30 15:23:07 -0700

    Create check_auth_changes.py

---
+++ process/atlassian_admin_process/check_auth_changes.py
@@ -0,0 +1,10 @@
+from logic.atlassian_admin_logic.auth_policies_logic import AuthPolicies
+
+auth_policies = AuthPolicies()
+
+auth_policies.get_status_of_task('29124fa2-ab9b-4573-8295-1ab4eee26b81')
+auth_policies.get_status_of_task('03afab9d-058c-4110-8d4e-861c5d4c770a')
+auth_policies.get_status_of_task('a2504828-f911-41bc-98a4-7c59e7000300')
+auth_policies.get_status_of_task('a09dc615-890d-45eb-b834-2fcea72744b2')
+auth_policies.get_status_of_task('3378a7d9-91b5-49a0-b265-b20c360c2b3e')
+auth_policies.get_status_of_task('cc12444b-961f-4bdd-a92c-438421522a62')
