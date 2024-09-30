257aea0f2df6276cff01700d8a6aa3dcab1b468d
mastervalor <mouradmarzouk63@gmail.com>
2024-09-30 09:23:07 -0700

    Update auth_policies_api.py

--- calls/atlassian_admin_api_calls/auth_policies_api.py
+++ calls/atlassian_admin_api_calls/auth_policies_api.py
@@ -8,3 +8,28 @@ class AtlassianAuthPolicies:
     def __init__(self):
         self.token = atlassian_admin_Bearer_token
         self.admin_url = atlassian_admin_v1
+
+    def add_user_to_policy(self, user, policy_id):
+        org_id = 'd816j2aj-j881-10a8-7c2c-10c7736ca181'
+        url = f"orgs/{org_id}/auth-policy/{policy_id}/add-users"
+
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": atlassian_admin_Bearer_token
+        }
+
+        payload = json.dumps({
+            "users": [
+                user
+            ]
+        })
+
+        response = json.loads(requests.request(
+            "POST",
+            url,
+            data=payload,
+            headers=headers
+        ).text)
+
+        return response
