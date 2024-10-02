2ebeb85bd8ce93ba15106cc344f2e97cc25956a3
mastervalor <mouradmarzouk63@gmail.com>
2024-10-01 23:02:43 -0700

    Create user_directory_logic.py

---
+++ logic/atlassian_admin_logic/user_directory_logic.py
@@ -0,0 +1,12 @@
+from calls.atlassian_admin_api_calls.user_directory_api_calls import UserDirectory
+
+
+class UserDirectoryLogic:
+    def __init__(self):
+        self.user_directory = UserDirectory()
+
+    def restore_users(self, users):
+        for user in users:
+            response = self.user_directory.restore_user(user)
+
+            
