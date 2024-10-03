237655d328b8f682f8635782b330314027104162
mastervalor <mouradmarzouk63@gmail.com>
2024-10-02 23:22:08 -0700

    Update auth_policies_api_tests.py

--- tests/atlassian_api_tests/auth_policies_api_tests.py
+++ tests/atlassian_api_tests/auth_policies_api_tests.py
@@ -6,4 +6,9 @@ class TestAuthPolicies(unittest.TestCase):
     def setUp(self):
         self.auth_policies = AtlassianAuthPolicies()
 
+    def test_add_users_to_policy(self):
+        users = ['test-user@getcruise.com']
+        new_policy = "3b93d6bc-1087-427f-b1a9-0dacc7bb837f"
+        response = self.auth_policies.add_users_to_policy(users, new_policy)
+        self.assertIn('taskId', response)
         
