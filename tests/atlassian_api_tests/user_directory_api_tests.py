79b79fb96625f552156f2951657c73e89a682c0f
mastervalor <mouradmarzouk63@gmail.com>
2024-10-06 23:21:40 -0700

    Create user_directory_api_tests.py

---
+++ tests/atlassian_api_tests/user_directory_api_tests.py
@@ -0,0 +1,9 @@
+import unittest
+from calls.atlassian_admin_api_calls.user_directory_api_calls import UserDirectory
+
+
+class TestUserDirectory(unittest.TestCase):
+    def setUp(self):
+        self.user_directory = UserDirectory()
+
+        
