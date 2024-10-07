e11036a6cd966132d5605c4e141b545d3c7ea3eb
mastervalor <mouradmarzouk63@gmail.com>
2024-10-06 22:59:14 -0700

    Update auth_policies_api_tests.py

--- tests/atlassian_api_tests/auth_policies_api_tests.py
+++ tests/atlassian_api_tests/auth_policies_api_tests.py
@@ -11,3 +11,13 @@ class TestAuthPolicies(unittest.TestCase):
         new_policy = "3b93d6bc-1087-427f-b1a9-0dacc7bb837f"
         response = self.auth_policies.add_users_to_policy(users, new_policy)
         self.assertIn('taskId', response)
+
+    def test_get_task_status(self):
+        task_id = 'f39e9fc7-1a83-47d0-9a82-f8e76f878558'
+        response = self.auth_policies.get_task_status(task_id).json()
+        self.assertIn('inProgressCount', response)
+
+    def test_invalid_get_task_status(self):
+        task_id = 'apple'
+        response = self.auth_policies.get_task_status(task_id)
+        self.assertEqual(response.status_code, 400)
