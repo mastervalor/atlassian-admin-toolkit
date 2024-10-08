53321736c4bc55e627bea933cd32417b0e8ea56b
mastervalor <mouradmarzouk63@gmail.com>
2024-10-08 14:25:32 -0700

    Update okta_users_api.py

--- calls/okta_api_calls/okta_users_api.py
+++ calls/okta_api_calls/okta_users_api.py
@@ -8,4 +8,19 @@ class OktaUsers:
     def __init__(self):
         self.token = okta_token
         self.okta_base_url = okta
+    
+    def users_profile(self, email):
+        url = self.okta_base_url + 'users'
+        params = {'filter': 'profile.email eq "{0}"'.format(email)}
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': self.token
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        try:
+            return json.loads(response.text)[0]
+        except IndexError:
+            return False
 
