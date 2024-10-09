e2f8e7b2ba30824b74d6b203e6819dcd190ba600
mastervalor <mouradmarzouk63@gmail.com>
2024-10-09 10:58:49 -0700

    Update check_users_status.py

--- process/okta_Process/check_users_status.py
+++ process/okta_Process/check_users_status.py
@@ -1,11 +1,21 @@
 from logic.okta_logic.okta_user_logic import OktaUsers
 from logic.os_logic.os_logic import OSLogic
 
-os_logic = OSLogic(open_file='suspended_users')
+os_logic = OSLogic(open_file='suspended_users', write_file='suspended_active_users')
 
 read_file = os_logic.read_file()
+active_users = []
 
 for user in read_file:
+    if user['User status'] == 'Suspended':
+        status = OktaUsers.get_user_status(user['email'])
+        if status == 'ACTIVE':
+            active_users.append({
+                'User id': user['User id'],
+                'Email': user['email'],
+                'Name': user['User name'],
+                'status': status
+            })
+            print(user['email'], status)
 
-    status = OktaUsers.get_user_status(user['email'])
-    print(user['email'], status)
+os_logic.write_to_file(active_users)
