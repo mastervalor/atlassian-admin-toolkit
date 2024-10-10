8201b7da7444871728ba50624a65ded20b38ef98
mastervalor <mouradmarzouk63@gmail.com>
2024-10-09 22:32:11 -0700

    Update response_handling.py

--- dataformating/response_handling.py
+++ dataformating/response_handling.py
@@ -1,4 +1,14 @@
 import json
+import requests
+
 
 class APIResponseHandler:
-    def __init__(self,)
+    def __init__(self, response):
+        self.json_data = self._parse_response()
+        self.response = response
+
+    def _parse_response(self):
+        try:
+            return self.response.json()
+        except ValueError:
+            return {"error": "Invalid JSON response"}
