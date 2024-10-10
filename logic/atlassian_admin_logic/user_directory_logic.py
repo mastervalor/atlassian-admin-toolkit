6be145d32e327a4b89945296e1bba9f4ca851504
mastervalor <mouradmarzouk63@gmail.com>
2024-10-10 10:16:13 -0700

    Update user_directory_logic.py

--- logic/atlassian_admin_logic/user_directory_logic.py
+++ logic/atlassian_admin_logic/user_directory_logic.py
@@ -1,4 +1,5 @@
 from calls.atlassian_admin_api_calls.user_directory_api_calls import UserDirectory
+from dataformating.response_handling import APIResponseHandler
 
 
 class UserDirectoryLogic:
@@ -8,6 +9,10 @@ class UserDirectoryLogic:
     def restore_users(self, users):
         for user in users:
             response = self.user_directory.restore_user(user)
-            print(response['message'])
+            handler = APIResponseHandler(response)
 
-
+            if handler.get_status_code() == 200:
+                print(f"Success: {handler.get_data().get('message', 'No message returned')}")
+            else:
+                print(f"Error: {handler.get_error_message()}")
+                
