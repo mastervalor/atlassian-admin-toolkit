9ec6aeeca56874199004499561f105d74ff18e8d
mastervalor <mouradmarzouk63@gmail.com>
2024-10-11 11:43:48 -0700

    Update looker_api_token_test.py

--- tests/looker_api_tests/looker_api_token_test.py
+++ tests/looker_api_tests/looker_api_token_test.py
@@ -1,3 +1,15 @@
 import unittest
 from calls.looker_api_calls.looker_token_api import LookerToken
 
+
+class TestLookerTokenApi(unittest.TestCase):
+    def setUp(self):
+        self.looker_token = LookerToken()
+
+    def test_get_access_token(self):
+        token = self.looker_token.get_access_token()
+        self.assertIsInstance(token, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
