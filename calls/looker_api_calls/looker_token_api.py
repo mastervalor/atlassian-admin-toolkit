34e697c8b70444ae463cd90badc3f29fb507dfbd
mastervalor <mouradmarzouk63@gmail.com>
2024-10-11 11:32:53 -0700

    Create looker_token_api.py

---
+++ calls/looker_api_calls/looker_token_api.py
@@ -0,0 +1,24 @@
+import requests
+from config import looker_base_url
+from auth import looker_client_id, looker_client_secret
+
+
+class LookerToken:
+    def __init__(self):
+        self.looker_url = looker_base_url
+        self.looker_client_id = looker_client_id
+        self.looker_client_secret = looker_client_secret
+
+    def get_access_token(self):
+        """Authenticate and return an access token."""
+        url = f'{looker_base_url}/api/4.0/login'
+        data = {
+            'client_id': self.looker_client_id,
+            'client_secret': self.looker_client_secret
+        }
+        response = requests.post(url, data=data)
+
+        if response.status_code == 200:
+            return response.json()['access_token']
+        else:
+            raise Exception(f'Failed to authenticate: {response.content}')
