4b44f01508dce742534605a752013813504dc4b3
mastervalor <mouradmarzouk63@gmail.com>
2024-10-11 14:23:52 -0700

    Update looker_dashboard_api.py

--- calls/looker_api_calls/looker_dashboard_api.py
+++ calls/looker_api_calls/looker_dashboard_api.py
@@ -1,17 +1,30 @@
 import requests
 import json
 from config import looker_base_url
-from looker_token_api import LookerToken
+from calls.looker_api_calls.looker_token_api import LookerToken
 
 
 class LookerDashboard:
     def __init__(self):
         self.looker_url = looker_base_url
-        self.token = LookerToken.get_access_token()
+        self.looker_token = LookerToken()
+        self.token = self.looker_token.get_access_token()
 
     def get_all_dashboards(self):
         """Retrieve all dashboards and their details."""
-        url = f'{looker_base_url}/api/4.0/dashboards'
+        url = f'{self.looker_url}/api/4.0/dashboards'
+        headers = {
+            'Authorization': f'token {self.token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.get(url, headers=headers)
+
+        return response
+
+    def get_dashboard_by_id(self, dashboard_id):
+        """Retrieve details of a specific dashboard by its ID."""
+        url = f'{self.looker_url}/api/4.0/dashboards/{dashboard_id}'
         headers = {
             'Authorization': f'token {self.token}',
             'Content-Type': 'application/json'
