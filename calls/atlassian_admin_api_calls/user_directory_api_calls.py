100e70ea7ef3e3ec1baa0562ced5cb1bf699576b
mastervalor <mouradmarzouk63@gmail.com>
2024-10-10 19:38:39 -0700

    Update user_directory_api_calls.py

--- calls/atlassian_admin_api_calls/user_directory_api_calls.py
+++ calls/atlassian_admin_api_calls/user_directory_api_calls.py
@@ -1,13 +1,13 @@
 import requests
 from auth import atlassian_admin_Bearer_token
 import json
-from config import atlassian_admin_v1
+from config import atlassian_org_api
 
 
 class UserDirectory:
     def __init__(self):
         self.token = atlassian_admin_Bearer_token
-        self.admin_url = atlassian_admin_v1
+        self.admin_url = atlassian_org_api
         self.org_id = 'd816j2aj-j881-10a8-7c2c-10c7736ca181'
 
     def restore_user(self, user_id):
