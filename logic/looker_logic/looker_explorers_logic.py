15cd9dce4a07a5236aa2ce0afc8cd82d15440649
mastervalor <mouradmarzouk63@gmail.com>
2024-10-12 17:33:55 -0700

    Create looker_explorers_logic.py

---
+++ logic/looker_logic/looker_explorers_logic.py
@@ -0,0 +1,16 @@
+from calls.looker_api_calls.looker_explores_api import LookerExplores
+from dataformating.response_handling import APIResponseHandler
+
+
+class LookerExplorersLogic:
+    def __init__(self):
+        self.looker_explorers = LookerExplores()
+
+    def get_all_explorers(self):
+        response = self.looker_explorers.get_all_explores()
+        handler = APIResponseHandler(response)
+
+        if handler.get_status_code() == 200:
+            print(f"Success: {handler.get_data().get('message', 'No message returned')}")
+        else:
+            print(f"Error code:{handler.get_status_code()} Error: {handler.get_error_message()}")
