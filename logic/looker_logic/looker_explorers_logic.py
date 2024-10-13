8f2b2f5fc487707c20b3dbbd20fc287dce52ebe3
mastervalor <mouradmarzouk63@gmail.com>
2024-10-12 22:26:26 -0700

    Update looker_explorers_logic.py

--- logic/looker_logic/looker_explorers_logic.py
+++ logic/looker_logic/looker_explorers_logic.py
@@ -6,7 +6,7 @@ class LookerExplorersLogic:
     def __init__(self):
         self.looker_explorers = LookerExplores()
 
-    def get_all_explorers(self):
+    def get_all_explores(self):
         response = self.looker_explorers.get_all_explores()
         handler = APIResponseHandler(response)
 
@@ -14,3 +14,9 @@ class LookerExplorersLogic:
             print(f"Success: {handler.get_data().get('message', 'No message returned')}")
         else:
             print(f"Error code:{handler.get_status_code()} Error: {handler.get_error_message()}")
+        return response
+
+    def get_explores_by_model(self, model_name):
+        response = self.looker_explorers.get_all_explores_by_model(model_name)
+
+        return response
