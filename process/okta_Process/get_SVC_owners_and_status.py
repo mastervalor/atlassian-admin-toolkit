6d63f32ad7390bb5e64ed2f702f30053aea5a803
mastervalor <mouradmarzouk63@gmail.com>
2024-10-15 19:33:25 -0700

    Update get_SVC_owners_and_status.py

--- process/okta_Process/get_SVC_owners_and_status.py
+++ process/okta_Process/get_SVC_owners_and_status.py
@@ -3,3 +3,9 @@ from logic.os_logic.os_logic import OSLogic
 
 
 okta_logic = OktaUsers()
+os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory', write_file='Jira API Tokens Inventory completed')
+file = os_logic.read_file()
+
+for row in file:
+    if row['Owner']:
+        print(row['Owner'])
