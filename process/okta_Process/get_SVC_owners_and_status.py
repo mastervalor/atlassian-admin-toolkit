7cd0f0979194388ff8dcd4472574ac29a2e7835b
mastervalor <mouradmarzouk63@gmail.com>
2024-10-15 23:13:58 -0700

    Update get_SVC_owners_and_status.py

--- process/okta_Process/get_SVC_owners_and_status.py
+++ process/okta_Process/get_SVC_owners_and_status.py
@@ -4,10 +4,22 @@ import re
 
 os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory', write_file='Jira API Tokens Inventory completed')
 file = os_logic.read_file()
+accounts = []
 
 for row in file:
     if not row['Owner']:
         svc = re.search(r'\((.*?)\)', row['API Integration Account']).group(1)
         owner = OktaUsers.get_user_second_email(svc)
-        owner_status = OktaUsers.get_user_status(owner)
-        print(owner, owner_status)
+        if owner:
+            owner_status = OktaUsers.get_user_status(owner)
+            if owner_status == 'DEPROVISIONED':
+                manager = OktaUsers.get_user_manager(owner)
+                if manager:
+                    manager_status = OktaUsers.get_user_status(manager)
+                    print(owner, owner_status, manager, manager_status)
+                else:
+                    print(owner, owner_status, manager)
+            else:
+                print(owner, owner_status)
+        else:
+            print(owner)
