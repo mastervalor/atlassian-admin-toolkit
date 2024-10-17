df341b90a18ecc1741495ad596cf3768e3a20752
mastervalor <mouradmarzouk63@gmail.com>
2024-10-17 15:45:53 -0700

    Create jira_api_projects.py

---
+++ calls/jira_api_calls/jira_api_projects.py
@@ -0,0 +1,10 @@
+import requests
+from auth import auth, staging_auth
+import json
+from config import jira, jira_staging
+
+
+class ProjectJiraCalls:
+    def __init__(self, is_staging=False):
+        self.token = staging_auth if is_staging else auth
+        self.jira = jira_staging if is_staging else jira
