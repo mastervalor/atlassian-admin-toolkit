b452cf67f2fde5acb58d2f6fa7f5a542a934aada
mastervalor <mouradmarzouk63@gmail.com>
2024-10-18 14:46:44 -0700

    Create looker_looks_api.py

---
+++ calls/looker_api_calls/looker_looks_api.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from config import looker_base_url
+from calls.looker_api_calls.looker_token_api import LookerToken
+
+
+class LooksExplores:
+    def __init__(self):
+        self.looker_url = looker_base_url
+        self.looker_token = LookerToken()
+        self.token = self.looker_token.get_access_token()
+
+    def search_looks_by_explore(self, explore_name):
+        headers = {'Authorization': f'token {self.token}'}
+        looks_url = f'{self.looker_url}/api/4.0/search_looks'
+        params = {'explore': explore_name}
+        response = requests.get(looks_url, headers=headers, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f'Failed to search looks by explore: {response.text}')
+        
