271673e8414641734fe1f68acd72d341cc643a8c
mastervalor <mouradmarzouk63@gmail.com>
2024-10-18 16:22:46 -0700

    Update looker_dashboards_logic.py

--- logic/looker_logic/looker_dashboards_logic.py
+++ logic/looker_logic/looker_dashboards_logic.py
@@ -51,3 +51,28 @@ class LookerDashboardLogic:
             return list(model_names)
         else:
             raise Exception(f'Failed to retrieve dashboard {dashboard_id}: {response.content}')
+
+    def search_for_jira_model(self, dashboard_elements):
+        jira_model_count = 0  # To count, how many times the "Jira" model appears
+        for element in dashboard_elements:
+            # Check in 'look' for the model
+            if 'look' in element:
+                look = element['look']
+                if 'query' in look and 'model' in look['query']:
+                    model_info = look['query']['model']
+                    if isinstance(model_info, dict) and model_info.get('id') == 'jira':
+                        jira_model_count += 1
+                    elif isinstance(model_info, str) and model_info == 'jira':
+                        jira_model_count += 1
+
+            # Check in 'result_maker' for the model
+            if 'result_maker' in element:
+                result_maker = element['result_maker']
+                if 'query' in result_maker and 'model' in result_maker['query']:
+                    model_info = result_maker['query']['model']
+                    if isinstance(model_info, dict) and model_info.get('id') == 'jira':
+                        jira_model_count += 1
+                    elif isinstance(model_info, str) and model_info == 'jira':
+                        jira_model_count += 1
+
+        return jira_model_count
