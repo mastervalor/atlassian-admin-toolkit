341c229e2d7dca3464e62f2b79c0658809c770fc
mastervalor <mouradmarzouk63@gmail.com>
2024-10-19 19:36:13 -0700

    Update looker_dashboard_info.py

--- process/looker_dashboards_process/looker_dashboard_info.py
+++ process/looker_dashboards_process/looker_dashboard_info.py
@@ -2,3 +2,13 @@ from logic.looker_logic.looker_dashboards_logic import LookerDashboardLogic
 from logic.os_logic.os_logic import OSLogic
 
 
+os_logic = OSLogic(open_file='looker dashboards', write_file='looker dashboards full')
+dashboard_logic = LookerDashboardLogic()
+dashboards = os_logic.read_file()
+dashboards_ids =[]
+
+for dash in dashboards:
+    dashboards_ids.append(dash['dashboard_id'])
+
+dashboards_info = dashboard_logic.get_dashboard_metadata(dashboards_ids)
+print(dashboards_ids)
