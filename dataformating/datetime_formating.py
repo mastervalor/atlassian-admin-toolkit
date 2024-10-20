deb54e9485486dc1c7e20efddd9f08ac05514900
mastervalor <mouradmarzouk63@gmail.com>
2024-10-19 18:03:17 -0700

    Update datetime_formating.py

--- dataformating/datetime_formating.py
+++ dataformating/datetime_formating.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
 class DateTimeFormating:
@@ -11,9 +12,12 @@ class DateTimeFormating:
         """
         if iso_string:
             try:
-                dt = datetime.fromisoformat(iso_string.replace('Z', '+00:00'))  # Handle ISO strings
-                return dt.strftime("%B %d, %Y, %I:%M %p UTC")
-            except ValueError:
+                dt = datetime.fromisoformat(iso_string.replace('Z', '+00:00'))  # Convert ISO to datetime
+                dt_utc = dt.astimezone(ZoneInfo("UTC"))  # Normalize to UTC
+                pt_timezone = ZoneInfo("America/Los_Angeles")  # Pacific Time (PT) timezone
+                dt_pt = dt_utc.astimezone(pt_timezone)  # Convert to Pacific Time
+                return dt_pt.strftime("%B %d, %Y, %I:%M %p %Z")  # Include timezone in output
+            except (ValueError, ZoneInfo.KeyError):
                 return iso_string  # Return as-is if there's an issue
         return "None"
 
