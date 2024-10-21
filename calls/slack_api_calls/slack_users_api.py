7d60b9e5c90c9e13aa00ff6490f6e64903322949
mastervalor <mouradmarzouk63@gmail.com>
2024-10-20 22:29:10 -0700

    Create slack_users_api.py

---
+++ calls/slack_api_calls/slack_users_api.py
@@ -0,0 +1,23 @@
+import requests
+from _post  import PostHandeling
+
+
+class UserAPIClient:
+    def __init__(self, token=None):
+        self.base_url = 'https://slack.com/api/'
+        self.token = token
+        self.post_handling = PostHandeling()
+
+    def get_user_id(self, user_email):
+        url = self.base_url + 'users.lookupByEmail'
+        headers = {
+            'Authorization': f'Bearer {self.token}'
+        }
+        params = {'email': user_email}
+        response = requests.get(url, headers=headers, params=params)
+        response_data = response.json()
+        if not response_data.get('ok'):
+            error = response_data.get('error', 'Unknown error')
+            print(f"Slack API error on users.lookupByEmail: {error}")
+            return None
+        return response_data['user']['id']
