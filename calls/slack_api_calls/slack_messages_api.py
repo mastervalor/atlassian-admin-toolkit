425354841dc399dd5f8ebd9015208b8ab0e58d41
mastervalor <mouradmarzouk63@gmail.com>
2024-10-22 16:33:55 -0700

    Update slack_messages_api.py

--- calls/slack_api_calls/slack_messages_api.py
+++ calls/slack_api_calls/slack_messages_api.py
@@ -2,7 +2,7 @@ from calls.slack_api_calls.slack_api_handling import SlackAPIHandling
 from calls.slack_api_calls.slack_users_api import SlackUserAPI
 
 
-class MessageHandler:
+class SlackMessageAPI:
     def __init__(self):
         self.api_handler = SlackAPIHandling()
         self.slack_users = SlackUserAPI()
