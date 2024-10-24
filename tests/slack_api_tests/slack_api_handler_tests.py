f5dab420b445fd52085d6494d67f035d765b6a71
mastervalor <mouradmarzouk63@gmail.com>
2024-10-23 22:50:17 -0700

    Update slack_api_handler_tests.py

--- tests/slack_api_tests/slack_api_handler_tests.py
+++ tests/slack_api_tests/slack_api_handler_tests.py
@@ -15,4 +15,14 @@ class TestSlackAPIHandling(unittest.TestCase):
         self.assertTrue(response['ok'])
         self.assertEqual(response['args']['foo'], 'bar')
 
-   
+    def test_post(self):
+        # Since 'api.test' supports both GET and POST, we can use it safely
+        endpoint = 'api.test'
+        data = {'foo': 'bar'}
+        response = self.api_handler.post(endpoint, data)
+        self.assertTrue(response['ok'])
+        self.assertEqual(response['args']['foo'], 'bar')
+
+
+if __name__ == '__main__':
+    unittest.main()
