13b99cfda8cd6ea7f32e91dc54a2d8f6c7830800
mastervalor <mouradmarzouk63@gmail.com>
2024-10-25 11:08:00 -0700

    Update slack_messages_logic.py

--- logic/slack_logic/slack_messages_logic.py
+++ logic/slack_logic/slack_messages_logic.py
@@ -7,11 +7,11 @@ class MessageLogic:
         self.user_handler = SlackUserAPI()
         self.message_handler = SlackMessageAPI()
 
-    def send_direct_message(self, user_email, message_text):
+    def send_direct_message(self, user_email, message_text=None, blocks=None):
         try:
             user_id = self.user_handler.get_user_id(user_email)
             channel_id = self.message_handler.open_direct_message(user_id)
-            self.message_handler.send_message(channel_id, message_text)
+            self.message_handler.send_message(channel_id, text=message_text, blocks=blocks)
             print(f"Message sent to {user_email}")
         except Exception as e:
             print(f"Failed to send direct message to {user_email}: {e}")
