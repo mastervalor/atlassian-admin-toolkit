44bc7084075e6c8f8ab5d7b56ef09cd16e51be48
mastervalor <mouradmarzouk63@gmail.com>
2024-10-28 15:48:31 -0700

    Update slack_automated_api_messages.py

--- process/slack_message_process/slack_automated_api_messages.py
+++ process/slack_message_process/slack_automated_api_messages.py
@@ -1,5 +1,5 @@
 from logic.slack_logic.slack_messages_logic import MessageLogic
-from process.slack_message_process.slack_message_templates import api_message_block, looker_message_block
+from process.slack_message_process.slack_message_templates import api_message_block
 from logic.os_logic.os_logic import OSLogic
 
 os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory')
@@ -7,14 +7,26 @@ os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory')
 file = os_logic.read_file()
 message_logic = MessageLogic()
 
-for i in file:
+for svc in file:
     user_emails = ['parag.hardas@getcruise.com', 'david.cooke@getcruise.com',
-                   'mourad.marzouk@getcruise.com']
+                   'mourad.marzouk@getcruise.com', 'satchidanand.challapalli@getcruise.com',
+                   'sumaiah.syed@getcruise.com', 'swaroop.vimalkumar@getcruise.com']
 
-    if i['Critical - needed on sunday'] != 'No':
-        if ',' in i['Owner']:
-            user_emails.extend(i['Owner'].split(','))
+    owner = [email.split('@')[0].replace('.', ' ') for email in svc['Owner'].split(',')]
+    if len(owner) == 1:
+        owner = owner[0]
+    elif len(owner) == 2:
+        owner = f"{owner[0]} and {owner[1]}"
+
+    svc_account = svc['API Integration Account']
+
+    if svc['Critical - needed on sunday'] != 'No':
+        if ',' in svc['Owner']:
+            user_emails.extend(svc['Owner'].split(','))
             print(user_emails)
         else:
-            user_emails.append(i['Owner'])
+            user_emails.append(svc['Owner'])
             print(user_emails)
+
+    blocks = api_message_block(owner, svc_account)
+    message_logic.send_group_message(user_emails, blocks=blocks)
