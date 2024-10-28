9c1fdd6c62d1a97e913718c998b63feea0885c16
mastervalor <mouradmarzouk63@gmail.com>
2024-10-28 14:30:48 -0700

    Update slack_automated_api_messages.py

--- process/slack_message_process/slack_automated_api_messages.py
+++ process/slack_message_process/slack_automated_api_messages.py
@@ -2,3 +2,11 @@ from logic.slack_logic.slack_messages_logic import MessageLogic
 from process.slack_message_process.slack_message_templates import api_message_block, looker_message_block
 from logic.os_logic.os_logic import OSLogic
 
+os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory')
+
+file = os_logic.read_file()
+
+for i in file:
+    if i['Critical - needed on sunday'] != 'No':
+        if 
+        print(i['Owner'])
