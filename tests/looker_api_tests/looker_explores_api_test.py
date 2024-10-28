03249e411acdb85c00e8b84658d340d7a255c927
mastervalor <mouradmarzouk63@gmail.com>
2024-10-27 21:33:43 -0700

    Update looker_explores_api_test.py

--- tests/looker_api_tests/looker_explores_api_test.py
+++ tests/looker_api_tests/looker_explores_api_test.py
@@ -6,3 +6,13 @@ class TestLookerExploresIntegration(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.looker_explores = LookerExplores()
+
+    def test_get_all_models(self):
+        response = self.looker_explores.get_all_models()
+
+        self.assertIsInstance(response, list, "Expected response to be a list of models")
+
+        if response:
+            model = response[0]
+            self.assertIn("name", model, "Each model should have a 'name' field")
+            self.assertIn("explores", model, "Each model should contain 'explores'")
