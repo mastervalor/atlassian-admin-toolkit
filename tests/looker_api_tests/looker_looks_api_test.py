a26bc3f51c52e5e7be5205ea3c2bd6b5679a766f
mastervalor <mouradmarzouk63@gmail.com>
2024-10-27 19:06:58 -0700

    Update looker_looks_api_test.py

--- tests/looker_api_tests/looker_looks_api_test.py
+++ tests/looker_api_tests/looker_looks_api_test.py
@@ -7,3 +7,16 @@ class TestLooksExploresIntegration(unittest.TestCase):
     def setUpClass(cls):
         cls.looks_explores = LooksExplores()
 
+    def test_search_looks_by_explore(self):
+        explore_name = "order_items"
+        response = self.looks_explores.search_looks_by_explore(explore_name)
+
+        self.assertIsInstance(response, list, "Expected response to be a list of looks")
+
+        if response:
+            look = response[0]
+            self.assertIn("id", look, "Each look result should contain an 'id' field")
+            self.assertIn("title", look, "Each look result should contain a 'title' field")
+            self.assertIn("query_id", look, "Each look result should contain a 'query_id' field")
+
+
