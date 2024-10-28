de60de59917f9974a0a8ab82ed77acdee7c51908
mastervalor <mouradmarzouk63@gmail.com>
2024-10-28 14:41:36 -0700

    Update slack_automated_api_messages.py

--- process/slack_message_process/slack_automated_api_messages.py
+++ process/slack_message_process/slack_automated_api_messages.py
@@ -5,8 +5,16 @@ from logic.os_logic.os_logic import OSLogic
 os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory')
 
 file = os_logic.read_file()
+message_logic = MessageLogic()
 
 for i in file:
+    user_emails = ['parag.hardas@getcruise.com', 'david.cooke@getcruise.com',
+                   'mourad.marzouk@getcruise.com']
+
     if i['Critical - needed on sunday'] != 'No':
-        if 
-        print(i['Owner'])
+        if ',' in i['Owner']:
+            user_emails.extend(i['Owner'].split(','))
+            print(user_emails)
+        else:
+            user_emails.append(i['Owner'])
+            print(user_emails)
