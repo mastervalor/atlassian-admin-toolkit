cb9718da00efb8874a7a680e394acf0f9e6a705e
mastervalor <mouradmarzouk63@gmail.com>
2024-10-27 18:57:24 -0700

    Update looker_user_api_test.py

--- tests/looker_api_tests/looker_user_api_test.py
+++ tests/looker_api_tests/looker_user_api_test.py
@@ -2,3 +2,8 @@ import unittest
 from calls.looker_api_calls.looker_user_api import LookerUsers
 
 
+class TestLookerUsersIntegration(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Initialize LookerUsers instance
+        cls.looker_users = LookerUsers()
