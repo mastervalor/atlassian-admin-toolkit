05fae382ffb63a6e980b39df4d8b1bc597b4a23e
mastervalor <mouradmarzouk63@gmail.com>
2024-10-30 11:40:40 -0700

    Update check_users_status_and_manager.py

--- process/okta_Process/check_users_status_and_manager.py
+++ process/okta_Process/check_users_status_and_manager.py
@@ -7,25 +7,21 @@ read_file = os_logic.read_file()
 active_users = []
 
 for user in read_file:
-    if user['User status'] == 'Suspended':
-        if user['created_by']:
-            creator_status = OktaUsers.get_user_status(user['created_by'])
-            if creator_status == 'ACTIVE':
-                active_users.append({
-                    'creator': user['created_by'],
-                    'status': creator_status
-                })
-                print(user['created_by'], creator_status)
+    if user['created_by']:
+        creator = user['created_by'].replace(' ', '.').lower()
+        print(creator)
+        # creator_status = OktaUsers.get_user_status(user['created_by'])
 
-        if user['last_updated_by']:
-            updater_status = OktaUsers.get_user_status(user['last_updated_by'])
-            if updater_status == 'ACTIVE':
-                active_users.append({
-                    'User id': user['User id'],
-                    'Email': user['email'],
-                    'Name': user['User name'],
-                    'status': updater_status
-                })
-                print(user['email'], updater_status)
+    if user['last_updated_by']:
+        updater = user['last_updated_by'].replace(' ', '.').lower()
+        print(updater)
+        # updater_status = OktaUsers.get_user_status(user['last_updated_by'])
 
-os_logic.write_to_file(active_users)
+        # if creator_status == 'ACTIVE':
+        #     active_users.append({
+        #         'creator': user['created_by'],
+        #         'status': creator_status
+        #     })
+        #     print(user['created_by'], creator_status)
+
+# os_logic.write_to_file(active_users)
