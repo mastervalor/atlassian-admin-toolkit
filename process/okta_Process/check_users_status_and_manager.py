a543fc05b942d9741b7589b1522c5953d5f5aa79
mastervalor <mouradmarzouk63@gmail.com>
2024-10-30 11:44:03 -0700

    Update check_users_status_and_manager.py

--- process/okta_Process/check_users_status_and_manager.py
+++ process/okta_Process/check_users_status_and_manager.py
@@ -9,13 +9,11 @@ active_users = []
 for user in read_file:
     if user['created_by']:
         creator = user['created_by'].replace(' ', '.').lower()
-        print(creator)
-        # creator_status = OktaUsers.get_user_status(user['created_by'])
+        creator_status = OktaUsers.get_user_status(user['created_by'])
 
     if user['last_updated_by']:
         updater = user['last_updated_by'].replace(' ', '.').lower()
-        print(updater)
-        # updater_status = OktaUsers.get_user_status(user['last_updated_by'])
+        updater_status = OktaUsers.get_user_status(user['last_updated_by'])
 
         # if creator_status == 'ACTIVE':
         #     active_users.append({
