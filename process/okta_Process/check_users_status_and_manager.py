b572e3b1fa185e52cf5ef6a0e551487416a0688d
mastervalor <mouradmarzouk63@gmail.com>
2024-10-30 11:09:15 -0700

    Update check_users_status_and_manager.py

--- process/okta_Process/check_users_status_and_manager.py
+++ process/okta_Process/check_users_status_and_manager.py
@@ -1,7 +1,7 @@
 from logic.okta_logic.okta_user_logic import OktaUsers
 from logic.os_logic.os_logic import OSLogic
 
-os_logic = OSLogic(open_file='suspended_users', write_file='suspended_active_users')
+os_logic = OSLogic(open_file='looker Final List', write_file='looker users')
 
 read_file = os_logic.read_file()
 active_users = []
