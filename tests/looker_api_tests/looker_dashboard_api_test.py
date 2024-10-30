934f8b1dc4bbc09db48e02624ad6c3762c529c44
mastervalor <mouradmarzouk63@gmail.com>
2024-10-29 22:58:48 -0700

    Update looker_dashboard_api_test.py

--- tests/looker_api_tests/looker_dashboard_api_test.py
+++ tests/looker_api_tests/looker_dashboard_api_test.py
@@ -1,2 +1,8 @@
 import unittest
 from calls.looker_api_calls.looker_dashboard_api import LookerDashboard
+
+class TestLookerDashboardIntegration(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Initialize LookerDashboard instance
+        cls.looker_dashboard = LookerDashboard()
