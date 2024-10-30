093fc81a500732e188093e707aa6ac0c8a790e33
mastervalor <mouradmarzouk63@gmail.com>
2024-10-30 11:25:00 -0700

    Update check_users_status_and_manager.py

--- process/okta_Process/check_users_status_and_manager.py
+++ process/okta_Process/check_users_status_and_manager.py
@@ -8,14 +8,26 @@ active_users = []
 
 for user in read_file:
     if user['User status'] == 'Suspended':
-        status = OktaUsers.get_user_status(user['email'])
-        if status == 'ACTIVE':
-            active_users.append({
-                'User id': user['User id'],
-                'Email': user['email'],
-                'Name': user['User name'],
-                'status': status
-            })
-            print(user['email'], status)
+        if user['created_by']:
+            creator_status = OktaUsers.get_user_status(user['created_by'])
+            if creator_status == 'ACTIVE':
+                active_users.append({
+                    'User id': user['User id'],
+                    'Email': user['email'],
+                    'Name': user['User name'],
+                    'status': creator_status
+                })
+                print(user['email'], creator_status)
+
+        if user['last_updated_by']:
+            updater_status = OktaUsers.get_user_status(user['last_updated_by'])
+            if updater_status == 'ACTIVE':
+                active_users.append({
+                    'User id': user['User id'],
+                    'Email': user['email'],
+                    'Name': user['User name'],
+                    'status': updater_status
+                })
+                print(user['email'], updater_status)
 
 os_logic.write_to_file(active_users)
