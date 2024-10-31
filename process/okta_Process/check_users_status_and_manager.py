9f126fe56c8c2861b5fa75edb05c3f1163c47765
mastervalor <mouradmarzouk63@gmail.com>
2024-10-30 17:30:11 -0700

    Update check_users_status_and_manager.py

--- process/okta_Process/check_users_status_and_manager.py
+++ process/okta_Process/check_users_status_and_manager.py
@@ -14,16 +14,16 @@ for user in read_file:
         manager, manager_title, manager_status = OktaUsers.get_manager_info(creator)
         board['creator'] = user['created_by']
         board['creator_status'] = creator_status
-        board['manager'] = manager
-        board['manager_title'] = manager_title
-        board['manager_status'] = manager_status
-        print(creator, creator_status)
+        board['creator_manager'] = manager
+        board['creator_manager_title'] = manager_title
+        board['creator_manager_status'] = manager_status
+        print("Creator and status: ",  creator, creator_status)
     else:
         board['creator'] = ' '
         board['creator_status'] = ' '
-        board['manager'] = ' '
-        board['manager_title'] = ' '
-        board['manager_status'] = ' '
+        board['creator_manager'] = ' '
+        board['creator_manager_title'] = ' '
+        board['creator_manager_status'] = ' '
 
     if user['last_updated_by']:
         updater = user['last_updated_by'].replace(' ', '.').lower()
@@ -31,16 +31,16 @@ for user in read_file:
         manager, manager_title, manager_status = OktaUsers.get_manager_info(updater)
         board['updater'] = user['last_updated_by']
         board['updater_status'] = updater_status
-        board['manager'] = manager
-        board['manager_title'] = manager_title
-        board['manager_status'] = manager_status
-        print(updater, updater_status)
+        board['updater_manager'] = manager
+        board['updater_manager_title'] = manager_title
+        board['updater_manager_status'] = manager_status
+        print("Updator and status: ", updater, updater_status)
     else:
         board['updater'] = ' '
         board['updater_status'] = ' '
-        board['manager'] = ' '
-        board['manager_title'] = ' '
-        board['manager_status'] = ' '
+        board['updater_manager'] = ' '
+        board['updater_manager_title'] = ' '
+        board['updater_manager_status'] = ' '
 
     active_users.append(board)
 
