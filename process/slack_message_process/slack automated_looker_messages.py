fea397728f22b1e70e8348518d4fb1f20a56e92f
mastervalor <mouradmarzouk63@gmail.com>
2024-10-31 23:26:54 -0700

    Update slack automated_looker_messages.py

--- process/slack_message_process/slack automated_looker_messages.py
+++ process/slack_message_process/slack automated_looker_messages.py
@@ -2,13 +2,16 @@ from logic.slack_logic.slack_messages_logic import MessageLogic
 from process.slack_message_process.slack_message_templates import api_message_block
 from logic.os_logic.os_logic import OSLogic
 
-os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory')
+os_logic = OSLogic(open_file='looker owners')
 
 file = os_logic.read_file()
 message_logic = MessageLogic()
 
-for svc in file:
-    user_emails = ['parag.hardas@getcruise.com', 'david.cooke@getcruise.com',
-                   'mourad.marzouk@getcruise.com', 'satchidanand.challapalli@getcruise.com',
-                   'swaroop.vimalkumar@getcruise.com']
+for board in file:
+    # user_emails = ['parag.hardas@getcruise.com', 'david.cooke@getcruise.com',
+    #                'mourad.marzouk@getcruise.com', 'satchidanand.challapalli@getcruise.com',
+    #                'swaroop.vimalkumar@getcruise.com']
+    if board['creator_status'] == 'DEPROVISIONED':
+        if board['creator_manager_status'] == 'ACTIVE':
+            print(board['creator_manager'])
 
