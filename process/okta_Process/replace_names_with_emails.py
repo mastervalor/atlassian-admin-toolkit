6430b687531b00aea1284cf628f9068b8bbc0f01
mastervalor <mouradmarzouk63@gmail.com>
2024-11-01 18:58:08 -0700

    Update replace_names_with_emails.py

--- process/okta_Process/replace_names_with_emails.py
+++ process/okta_Process/replace_names_with_emails.py
@@ -1,8 +1,13 @@
 from logic.okta_logic.okta_user_logic import OktaUsers
 from logic.os_logic.os_logic import OSLogic
 
-os_logic = OSLogic(open_file='looker owners')
+os_logic = OSLogic(open_file='looker owners', write_file='looker owners emails')
 
 file = os_logic.read_file()
+
+for row in file:
+    if row['creator']:
+        creator = row['creator'].replace(' ', '.')
+        creator_email = OktaUsers.get_user_email(creator)
 
 print(OktaUsers.get_user_email('adam.bowser'))
