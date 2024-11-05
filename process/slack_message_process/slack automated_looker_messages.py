b33d13862790f8cb19670c556a19b256e4c1f626
mastervalor <mouradmarzouk63@gmail.com>
2024-11-04 16:36:01 -0800

    Update slack automated_looker_messages.py

--- process/slack_message_process/slack automated_looker_messages.py
+++ process/slack_message_process/slack automated_looker_messages.py
@@ -1,5 +1,5 @@
 from logic.slack_logic.slack_messages_logic import MessageLogic
-from process.slack_message_process.slack_message_templates import api_message_block
+from process.slack_message_process.slack_message_templates import looker_message_block, looker_inactive_message_block
 from logic.os_logic.os_logic import OSLogic
 
 os_logic = OSLogic(open_file='looker owners')
@@ -11,10 +11,23 @@ for board in file:
     # user_emails = ['parag.hardas@getcruise.com', 'david.cooke@getcruise.com',
     #                'mourad.marzouk@getcruise.com', 'satchidanand.challapalli@getcruise.com',
     #                'swaroop.vimalkumar@getcruise.com']
+
+    dashboard_name = board['title']
+    dashboard_id = board['dashboard_id']
     if board['Needed?'] == 'N':
-        if board['creator'] == 'Active':
+        if board['creator_status'] == 'Active':
             creator = board['creator']
         elif board['creator_manager_status'] == 'ACTIVE':
             creator = board['creator_manager']
         else:
             creator = None
+
+        if board['updater_status'] == 'Active':
+            updater = board['updater']
+        elif board['updater_manager_status'] == 'ACTIVE':
+            updater = board['updater_manager']
+        else:
+            updater = None
+
+        message_block = looker_inactive_message_block()
+
