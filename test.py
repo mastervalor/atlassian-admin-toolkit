4e9c884260ea2c95ee5d2512218ecded97b7dd5b
mastervalor <mouradmarzouk63@gmail.com>
2024-11-25 23:44:41 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,34 @@
-from calls.looker_api_calls.looker_dashboard_api import LookerDashboard
-from calls.looker_api_calls.looker_explores_api import LookerExplores
-from logic.looker_logic.looker_dashboards_logic import LookerDashboardLogic
-from logic.looker_logic.looker_explorers_logic import LookerExplorersLogic
-from logic.okta_logic.okta_user_logic import OktaUsers
-from dataformating.json_formating import JSONFormating
-import json
+from calls.slack_api_calls.slack_api_handling import SlackAPIHandling
+from logic.slack_logic.slack_messages_logic import MessageLogic
+from process.slack_message_process.slack_message_templates import api_message_block, looker_message_block
+from logic.os_logic.os_logic import OSLogic
 
-okta_logic = OktaUsers()
-looker_dashboard = LookerDashboard()
-looker_explorer = LookerExplores()
-explorer = LookerExplorersLogic()
-dashboard_logic = LookerDashboardLogic()
-dashboard_ids = ['1']
-dashboard_id = 'ra-db::ra_db_ra_bugs'
-model_name = 'metrics'
-# response = looker_dashboard.get_dashboard_by_id(dashboard_id).json()
-response = looker_dashboard.get_dashboard_by_id(dashboard_id).json()
-# response = looker_dashboard.search_dashboards_for_explore('jira_issues')
-# response2 = dashboard.get_dashboards_models(dashboard_id, 'metrics')
-# response = looker_explorer.get_all_explores_by_model('jira')
-# response = looker_explorer.get_all_explores().json()
-# response = json.loads(response.text)
-# response = okta_logic.get_user_second_email('tars.svc@getcruise.com')
-# response = looker_explorer.get_query_history('jira', 'Stability_Buckets_SLA')
-# response = looker_dashboard.get_all_dashboards().json
-JSONFormating.pretty_json(response)
-# print(response2)
+
+os_logic = OSLogic(open_file='looker owners')
+
+file = os_logic.read_file()
+message_logic = MessageLogic()
+
+for board in file:
+    user_emails = []
+    first_names =[]
+    if board['creator_status'] == 'Active':
+        user_emails.append(board['creator'])
+        creator = board['creator'].split("@")[0]
+    elif board['creator_manager_status'] == 'ACTIVE':
+        user_emails.append(board['creator_manager'])
+        creator = board['creator_manager'].split("@")[0]
+    else:
+        creator = None
+
+    if board['updater_status'] == 'Active':
+        user_emails.append(board['updater'])
+        updater = board['updater'].split("@")[0]
+    elif board['updater_manager_status'] == 'ACTIVE':
+        user_emails.append(board['updater_manager'])
+        updater = board['updater_manager'].split("@")[0]
+    else:
+        updater = None
+
+    print(user_emails)
+    print(first_names)
