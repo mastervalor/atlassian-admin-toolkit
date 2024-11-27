84c2e5b344801bbd56e5032a9e7585a7ab781bd3
mastervalor <mouradmarzouk63@gmail.com>
2024-11-27 14:22:07 -0800

    Update slack_automated_api_messages.py

--- process/slack_message_process/slack_automated_api_messages.py
+++ process/slack_message_process/slack_automated_api_messages.py
@@ -1,10 +1,10 @@
 from logic.slack_logic.slack_messages_logic import MessageLogic
 from process.slack_message_process.slack_message_templates import api_message_block
-from logic.os_logic.os_logic import OSLogic
+from logic.os_logic.csv_logic import CSVLogic
 
-os_logic = OSLogic(open_file='Jira API Integration Tokens Inventory')
+csv_logic = CSVLogic(open_file='Jira API Integration Tokens Inventory')
 
-file = os_logic.read_file()
+file = csv_logic.read_file()
 message_logic = MessageLogic()
 
 for svc in file:
