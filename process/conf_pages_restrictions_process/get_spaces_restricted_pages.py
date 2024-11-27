47e1f4a8fca2c7a7e6eee7be51f4fff8fe781050
mastervalor <mouradmarzouk63@gmail.com>
2024-11-27 14:46:46 -0800

    Update get_spaces_restricted_pages.py

--- process/conf_pages_restrictions_process/get_spaces_restricted_pages.py
+++ process/conf_pages_restrictions_process/get_spaces_restricted_pages.py
@@ -1,10 +1,10 @@
 from logic.confluence_logic.space_logic import Spaces
-from logic.os_logic.os_logic import OSLogic
+from logic.os_logic.csv_logic import CSVLogic
 
 # get pages Ids from a list of names
 spaces = Spaces()
-os_logic = OSLogic(open_file='LucidChart', write_file='lucid chart page ids', columns=['Page ID'])
-lucid_chart = os_logic.read_file()
+csv_logic = CSVLogic(open_file='LucidChart', write_file='lucid chart page ids', columns=['Page ID'])
+lucid_chart = csv_logic.read_file()
 spaces_list = []
 page_ids = []
 
@@ -21,4 +21,4 @@ for key,value in space_ids.items():
     print(f"Space: {key}, ID: {value} had restricted pages: {len(restricted_pages)}")
 
 data_rows = [{'Page ID': page_id} for page_id in page_ids]
-os_logic.write_to_file(data_rows)
+csv_logic.write_to_file(data_rows)
