001bb2b50d11fc9158db8f9243aba3b86c124ff8
mastervalor <mouradmarzouk63@gmail.com>
2024-11-27 14:29:03 -0800

    Update userGroup.py

--- process/okta_Process/userGroup.py
+++ process/okta_Process/userGroup.py
@@ -1,15 +1,15 @@
-from logic.os_logic.os_logic import OSLogic
+from logic.os_logic.csv_logic import CSVLogic
 from logic.jira_logic.group_logic import Groups
 from logic.jira_logic.user_logic import Users
 
 columns = ["learn agents", "other groups"]
 
-os_logic = OSLogic(open_file='learn agents', append_file='learn agents groups', columns=columns)
+csv_logic = CSVLogic(open_file='learn agents', append_file='learn agents groups', columns=columns)
 groups = Groups()
 users = Users()
-file = os_logic.read_file()
+file = csv_logic.read_file()
 
 for name in file:
     user_group = users.user_groups(name['LEARN agents '])
     agent_groups = [group for group in user_group if group.endswith("-agent")]
-    os_logic.append_to_file([name['LEARN agents '], agent_groups])
+    csv_logic.append_to_file([name['LEARN agents '], agent_groups])
