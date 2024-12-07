c8c9dada492449ead24a3d530bf5f031507a47a4
mastervalor <mouradmarzouk63@gmail.com>
2024-12-06 16:48:03 -0800

    Update search_json_file.py

--- process/json_manip/search_json_file.py
+++ process/json_manip/search_json_file.py
@@ -6,27 +6,20 @@ csv_files = CSVLogic(open_file='Classic-CustomField')
 data = json_logic.json_file_manip.read_file()
 fields_file = csv_files.read_file()
 
-if data:
-    search_term = 'customfield_'
+search_term = 'customfield_'
 
-    total_occurrences = json_logic.count_occurrences(data, search_term)
+total_occurrences = json_logic.count_occurrences(data, search_term)
 
-    print(f"Total occurrences of '{search_term}' in 'automation-rules.json': {total_occurrences}")
-else:
-    print("Failed to read data from 'automation-rules.json'")
-
-if data:
-    search_pattern = r'customfield_\d+'
-
-    matches = json_logic.find_occurrences(data, search_pattern)
-
-    unique_matches = set(matches)
-
-    print(f"Found the following custom fields in 'automation-rules.json':")
-    for match in unique_matches:
-        field_id = match.split('customfield_')[1]
-        print(field_id)
-else:
-    print("Failed to read data from 'automation-rules.json'")
+print(f"Total occurrences of '{search_term}' in 'automation-rules.json': {total_occurrences}")
 
 
+search_pattern = r'customfield_\d+'
+
+matches = json_logic.find_occurrences(data, search_pattern)
+
+unique_matches = set(matches)
+
+print(f"Found the following custom fields in 'automation-rules.json':")
+for match in unique_matches:
+    field_id = match.split('customfield_')[1]
+    print(field_id)
