06f506f4dd4cd3b31a5868542a807de2f0522d92
mastervalor <mouradmarzouk63@gmail.com>
2025-01-24 11:05:49 -0800

    Update jira_api_tickets.py

--- calls/jira_api_calls/jira_api_tickets.py
+++ calls/jira_api_calls/jira_api_tickets.py
@@ -8,3 +8,23 @@ class TicketsJiraCalls:
     def __init__(self, is_staging=False):
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
+
+    def create_ticket(self, ticket):
+        url = self.jira + 'issue'
+
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        payload = json.dumps(ticket)
+
+        response = json.loads(requests.request(
+            "POST",
+            url,
+            headers=headers,
+            data=payload,
+            auth=self.token
+        ).text)
+
+        return response
+    
