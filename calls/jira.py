ed4604a44006aeafbae915a01e61c9b95c283f30
mastervalor <mouradmarzouk63@gmail.com>
2025-01-23 20:39:24 -0800

    Update jira.py

--- calls/jira.py
+++ calls/jira.py
@@ -9,22 +9,6 @@ class Jira:
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
 
-    def get_workflows(self):
-        url = self.jira + "workflow"
-
-        headers = {
-            "Accept": "application/json"
-        }
-
-        response = json.loads(requests.request(
-            "GET",
-            url,
-            headers=headers,
-            auth=self.token
-        ).text)
-
-        return response
-
     def get_custom_field_context(self, fieldId, contextId):
         url = self.jira + f'customFields/{fieldId}/context/{contextId}'
 
