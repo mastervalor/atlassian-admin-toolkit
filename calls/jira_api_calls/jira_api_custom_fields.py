a2cf133f820c256839d9ce48b573d8889cae389c
mastervalor <mouradmarzouk63@gmail.com>
2025-01-23 22:09:53 -0800

    Update jira_api_custom_fields.py

--- calls/jira_api_calls/jira_api_custom_fields.py
+++ calls/jira_api_calls/jira_api_custom_fields.py
@@ -8,3 +8,19 @@ class ProjectJiraCalls:
     def __init__(self, is_staging=False):
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
+
+    def get_custom_field_context(self, fieldId, contextId):
+        url = self.jira + f'customFields/{fieldId}/context/{contextId}'
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        response = json.loads(requests.request(
+            "GET",
+            url,
+            headers=headers,
+            auth=self.token
+        ).text)
+
+        return response
