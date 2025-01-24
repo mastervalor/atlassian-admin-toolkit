adfc0edd2b27c689165e5ca3abdc81cc86988a57
mastervalor <mouradmarzouk63@gmail.com>
2025-01-24 11:41:17 -0800

    Update targetedOwners.py

--- targetedOwners.py
+++ targetedOwners.py
@@ -1,13 +1,15 @@
 import json
-from calls. jira_api_calls.jira_api_projects import ProjectJiraCalls
+from calls.jira_api_calls.jira_api_projects import ProjectJiraCalls
+from calls.jira_api_calls.jira_api_tickets import TicketsJiraCalls
 
 projects = ProjectJiraCalls()
+ticket_calls = TicketsJiraCalls()
 archive_list = []
 
 jql = ('project = "Corporate Engineering" and summary ~ "does not meet the new requirements, and will be targeted for '
        'archiving" and status in ("Waiting for Response", Backlog, "In Progress")')
 
-tickets = jira.jql('?startAt=0&maxResults=100', jql)
+tickets = ticket_calls.jql('?startAt=0&maxResults=100', jql)
 
 for ticket in tickets['issues']:
     description = ticket['fields']['description']
@@ -21,6 +23,5 @@ for project in projects:
     if project['key'] not in archive_list:
         owner, status = projects.project_owner(project['key'])
         print(status)
-
 
 # print(json.dumps(projects, sort_keys=True, indent=4, separators=(",", ": ")))
