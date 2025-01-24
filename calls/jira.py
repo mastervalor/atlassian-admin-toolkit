fb35ac11dc715a8cb975ec5fc7c1ea71333caf3e
mastervalor <mouradmarzouk63@gmail.com>
2025-01-24 11:16:17 -0800

    Update jira.py

--- calls/jira.py
+++ calls/jira.py
@@ -9,25 +9,6 @@ class Jira:
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
 
-    def create_ticket(self, ticket):
-        url = self.jira + 'issue'
-
-        headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-        payload = json.dumps(ticket)
-
-        response = json.loads(requests.request(
-            "POST",
-            url,
-            headers=headers,
-            data=payload,
-            auth=self.token
-        ).text)
-
-        return response
-
     def jql(self, pref, payload):
         url = self.jira + 'search' + pref
 
