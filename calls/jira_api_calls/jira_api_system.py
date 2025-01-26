9e6ad31b0458c21b85451e772fcf2772c35eb10a
mastervalor <mouradmarzouk63@gmail.com>
2025-01-26 10:44:34 -0800

    Create jira_api_system.py

---
+++ calls/jira_api_calls/jira_api_system.py
@@ -0,0 +1,10 @@
+import requests
+from auth import auth, staging_auth
+import json
+from config import jira, jira_staging
+
+
+class SystemJiraCalls:
+    def __init__(self, is_staging=False):
+        self.token = staging_auth if is_staging else auth
+        self.jira = jira_staging if is_staging else jira
