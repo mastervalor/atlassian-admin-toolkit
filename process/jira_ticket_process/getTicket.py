9453fe63e73ca480cafb55234b13c4d34f95ff40
mastervalor <mouradmarzouk63@gmail.com>
2025-01-25 23:05:30 -0800

    Update getTicket.py

--- process/jira_ticket_process/getTicket.py
+++ process/jira_ticket_process/getTicket.py
@@ -1,9 +1,9 @@
 import json
-from calls.jira import Jira
+from calls.jira_api_calls.jira_api_tickets import TicketsJiraCalls
 
-jira = Jira()
+tickets = TicketsJiraCalls()
 
-response = jira.get_ticket("BRM-163")
+response = tickets.get_ticket("BRM-163")
 
 # owner = response['fields']['status']['name']
 # res = response['fields']['resolution']['name']
