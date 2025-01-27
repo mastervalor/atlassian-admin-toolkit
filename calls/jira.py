90e9203632199ad8f9c6f6b3d4a5eda1120ab0cb
mastervalor <mouradmarzouk63@gmail.com>
2025-01-27 13:15:03 -0800

    Update jira.py

--- calls/jira.py
+++ calls/jira.py
@@ -8,27 +8,3 @@ class Jira:
     def __init__(self, is_staging=False):
         self.token = staging_auth if is_staging else auth
         self.jira = jira_staging if is_staging else jira
-
-    def post_issue_type_scheme(self, scheme, keys):
-        url = self.jira + 'issuetypescheme/' + scheme + '/associations'
-
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        payload = {
-            'idsOrKeys': keys
-        }
-
-        response = json.loads(requests.request(
-            "POST",
-            url,
-            headers=headers,
-            json=payload,
-            auth=self.token
-        ).text)
-
-        return response
-
-
-    def update_ticket(self, key, payload):
-        url = self.jira + 'issue/' + key
