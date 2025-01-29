e5e847f58ae63b1ee2a9278ab482430d6749996c
mastervalor <mouradmarzouk63@gmail.com>
2025-01-29 15:10:58 -0800

    Update conf_api_users.py

--- calls/confluence_api_calls/conf_api_users.py
+++ calls/confluence_api_calls/conf_api_users.py
@@ -10,4 +10,17 @@ class ConfluenceSpaceCalls:
         self.cloud_v2 = conf_cloud_v2_dev if is_staging else conf_cloud_v2
         self.token = conf_cloud_dev_token
 
-        
+    def get_user(self, username):
+        url = self.cloud_v1 + f'user?username={username}'
+        headers = {
+            "Authorization": self.token,
+            "Content-Type": "application/json"}
+        print(url)
+        print(headers)
+        response = json.loads(requests.request(
+            "GET",
+            url,
+            headers=headers,
+        ).text)
+
+        return response
