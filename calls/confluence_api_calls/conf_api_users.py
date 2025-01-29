24dbde7debdcb79166bca8990daa8ecb7f0f3dd8
mastervalor <mouradmarzouk63@gmail.com>
2025-01-29 15:09:56 -0800

    Create conf_api_users.py

---
+++ calls/confluence_api_calls/conf_api_users.py
@@ -0,0 +1,13 @@
+import requests
+from auth import conf_cloud_dev_token
+import json
+from config import conf_cloud_v1, conf_cloud_v2, conf_cloud_v1_dev, conf_cloud_v2_dev
+
+
+class ConfluenceSpaceCalls:
+    def __init__(self, is_staging=False):
+        self.cloud_v1 = conf_cloud_v1_dev if is_staging else conf_cloud_v1
+        self.cloud_v2 = conf_cloud_v2_dev if is_staging else conf_cloud_v2
+        self.token = conf_cloud_dev_token
+
+        
