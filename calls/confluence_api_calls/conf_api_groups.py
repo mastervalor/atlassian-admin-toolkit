b0fdc2bdd2d4340e85ac58deca3ab6312ebf4c31
mastervalor <mouradmarzouk63@gmail.com>
2025-01-29 15:14:31 -0800

    Create conf_api_groups.py

---
+++ calls/confluence_api_calls/conf_api_groups.py
@@ -0,0 +1,25 @@
+import requests
+from auth import conf_cloud_dev_token
+import json
+from config import conf_cloud_v1, conf_cloud_v2, conf_cloud_v1_dev, conf_cloud_v2_dev
+
+
+class ConfluenceGroupsCalls:
+    def __init__(self, is_staging=False):
+        self.cloud_v1 = conf_cloud_v1_dev if is_staging else conf_cloud_v1
+        self.cloud_v2 = conf_cloud_v2_dev if is_staging else conf_cloud_v2
+        self.token = conf_cloud_dev_token
+
+    def group_members(self, pref):
+        url = self.cloud_v1 + pref
+        headers = {
+            "Authorization": self.token,
+            "Content-Type": "application/json"}
+
+        response = json.loads(requests.request(
+            "GET",
+            url,
+            headers=headers,
+        ).text)
+
+        return response
