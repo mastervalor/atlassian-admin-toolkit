6a7af4dfea2615f7025bc0202a52baa91447073d
mastervalor <mouradmarzouk63@gmail.com>
2025-01-29 15:21:01 -0800

    Update jiraGroupMembers.py

--- jiraGroupMembers.py
+++ jiraGroupMembers.py
@@ -1,14 +1,14 @@
-from logic.jira_logic.group_logic import GroupsUsers
+from logic.jira_logic.group_logic import Groups
 import csv
 import os
 
-jira = GroupsUsers()
+jira_groups = Groups()
 newFile = "app-jira members"
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
     writer = csv.writer(new_csv)
     writer.writerow(['username'])
-    response = jira.group_members('app-jira')
+    response = jira_groups.group_members('app-jira')
     for member in response:
         writer.writerow([member])
         print(member)
