6df72d893439eca71bf70bb73dc68703f3b70490
mastervalor <mouradmarzouk63@gmail.com>
2025-01-30 18:44:20 -0800

    Update moveTreeBranch.py

--- moveTreeBranch.py
+++ moveTreeBranch.py
@@ -1,6 +1,6 @@
-from call import Confluence
+from calls.confluence_api_calls.conf_api_pages import ConfluencePageCalls
 import json
 
-
-page_tree = Confluence.get_child_pages_recursive('324733855')
+conf_pages = ConfluencePageCalls()
+page_tree = conf_pages.get_child_pages_recursive('324733855')
 print(json.dumps(page_tree, sort_keys=True, indent=4, separators=(",", ": ")))
