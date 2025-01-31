9940d0cb05d6e02678672a30d09a11c677bb1227
mastervalor <mouradmarzouk63@gmail.com>
2025-01-31 11:32:53 -0800

    Update confluence.py

--- calls/confluence.py
+++ calls/confluence.py
@@ -10,24 +10,4 @@ class Confluence:
         self.conf_url = confluence_staging if is_staging else confluence
         self.conf_base = conf_base
 
-    def delete_page(self, page_id):
-        url = self.conf_url + f"content/{page_id}"
 
-        headers = {
-            'Authorization': self.token,
-            'Content-Type': 'application/json'
-        }
-
-        response = json.loads(requests.request(
-            "DELETE",
-            url,
-            headers=headers
-        ).text)
-
-        if response.status_code == 200:
-            print(f"Page '{page_id}' deleted successfully!")
-        else:
-            print(f"Failed to delete page. Status code: {response.status_code}")
-            print(response.text)
-            return False
-        return True
