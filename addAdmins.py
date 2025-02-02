19a8f020e93db9931566930abf505f29294d46cc
mastervalor <mouradmarzouk63@gmail.com>
2025-02-02 15:35:31 -0800

    Update addAdmins.py

--- addAdmins.py
+++ addAdmins.py
@@ -1,9 +1,5 @@
 from logic.jira_logic.project_logic import Projects
-import csv
-import os
-import requests
-import json
-from auth import auth, staging_auth
+
 
 project_logic = Projects()
 group = "administrators"
