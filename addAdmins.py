b18b8cc6e7ce03a30255e331fb99d762c54955a2
mastervalor <mouradmarzouk63@gmail.com>
2025-02-01 23:21:34 -0800

    Update addAdmins.py

--- addAdmins.py
+++ addAdmins.py
@@ -1,31 +1,12 @@
-from call import call
+from logic.jira_logic.project_logic import Projects
 import csv
 import os
 import requests
 import json
 from auth import auth, staging_auth
 
-# openFile = "Projects No Admin Role"
-# with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), openFile), mode='r') as csv_file:
-#     csv_reader = csv.DictReader(csv_file)
-#     for key in csv_reader:
-url = f"https://jira.robot.car/rest/api/2/project/plm/role/10002"
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
-payload = json.dumps({
-    "group": [
-        "administrators"
-    ]
-})
+project_logic = Projects()
+group = "administrators"
+project_key = ''
 
-response = requests.request(
-    "POST",
-    url,
-    data=payload,
-    headers=headers,
-    auth=auth
-)
-
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+print(project_logic.add_group_admins_to_project(group, project_key))
