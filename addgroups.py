f55502db34f9cf6625a0a82e2aec8f8c9c988d15
mastervalor <mouradmarzouk63@gmail.com>
2025-02-02 20:51:52 -0800

    Update addgroups.py

--- addgroups.py
+++ addgroups.py
@@ -1,45 +1,20 @@
-import requests
-from auth import auth
 from logic.jira_logic.project_logic import Projects
-import json
 from group import check_group
-import csv
-import os
-
-
-def call(ext):
-    url = "https://lucidmotors.atlassian.net/rest/api/3/" + ext
-
-    headers = {
-        "Accept": "application/json"
-    }
-
-    response = json.loads(requests.request(
-        "GET",
-        url,
-        headers=headers,
-        auth=auth
-    ).text)
-    return response
 
 
 projectRoles = ['10001', '10002', '10301', '10000', '10300', '10425', '10432']
 projectType = ['developers', 'admins', 'agents', 'users', 'customers', 'suppliers', 'read-only']
-projects = call('project')
 project_logic = Projects()
+project_keys = project_logic.get_active_all_project_keys()
 
-for i in projects:
-    if i['name'].startswith("[dead]") or i['name'].startswith("{Archived}") or i['name'].startswith("{ARCHIVE}") \
-            or ("archived" in i and i['archived'] == 'True'):
-        continue
-    else:
-        for (t, l) in zip(projectType, projectRoles):
-            group = f'okta_jira_{i["key"]}_{t}'
-            if check_group(group):
-                response = project_logic.add_group_to_project_by_role(group, l, i['key'])
-                if response.status_code == 200:
-                    print(f"This group: {group}, was added to project: {i['key']} with the role of {t}")
-                elif response.status_code == 400:
-                    print(f"This group: {group}, did not match to project: {i['key']} with the role of {t}")
-                else:
-                    print(response)
+for i in project_keys:
+    for (t, l) in zip(projectType, projectRoles):
+        group = f'okta_jira_{i["key"]}_{t}'
+        if check_group(group):
+            response = project_logic.add_group_to_project_by_role(group, l, i['key'])
+            if response.status_code == 200:
+                print(f"This group: {group}, was added to project: {i['key']} with the role of {t}")
+            elif response.status_code == 400:
+                print(f"This group: {group}, did not match to project: {i['key']} with the role of {t}")
+            else:
+                print(response)
