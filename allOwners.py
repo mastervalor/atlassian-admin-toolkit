7f11ffb637f6cb2fcf6c43c1ebd72221f0ee3027
mastervalor <mouradmarzouk63@gmail.com>
2025-02-08 23:28:50 -0800

    Delete allOwners.py

--- allOwners.py
+++
@@ -1,7 +0,0 @@
-from call import call, conf_call
-import json
-
-
-response = conf_call('space?limit=1000&type=global&status=current&expand=description.plain.value')
-
-print(json.dumps(response, sort_keys=True, indent=4, separators=(",", ": ")))
