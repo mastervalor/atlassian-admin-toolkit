a407b67c1fac749d5201649a95d79dd94162498b
mastervalor <mouradmarzouk63@gmail.com>
2025-02-09 14:37:00 -0800

    Delete addAdmins.py

--- process/jira_project_process/addAdmins.py
+++
@@ -1,8 +0,0 @@
-from logic.jira_logic.project_logic import Projects
-
-
-project_logic = Projects()
-group = "administrators"
-project_key = ''
-
-print(project_logic.add_group_admins_to_project(group, project_key))
