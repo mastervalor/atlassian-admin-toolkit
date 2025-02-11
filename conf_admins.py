7db7c414aff81ea45090fa8927c630c3064fef0f
mastervalor <mouradmarzouk63@gmail.com>
2025-02-10 16:19:24 -0800

    Update conf_admins.py

--- conf_admins.py
+++ conf_admins.py
@@ -1,16 +1,17 @@
 import csv
 import os
 from calls.okta_api_calls.okta_users_api import OktaUsersCalls
-from call import conf_call
+from calls.confluence_api_calls.conf_api_groups import ConfluenceGroupsCalls
 
 admins = ['administrators', 'app-confluence-admin', 'con-coyote-creek', 'con-coyote-creek-confluence', 'system-administrators']
 newFile = 'Conf-admins'
+conf_groups = ConfluenceGroupsCalls()
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
     writer = csv.writer(new_csv)
     writer.writerow(['Email', 'Group', 'Global Permissions', 'Manager'])
     for j in admins:
-        response = conf_call(f'group/{j}/member')
+        response = conf_groups.group_members(j)
 
         for i in response['results']:
             email = f'{i["username"]}@getcruise.com'
