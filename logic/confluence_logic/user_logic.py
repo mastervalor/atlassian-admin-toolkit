3888d46a7a820a2a6a66125ff4dc8a362802b0b0
mastervalor <mouradmarzouk63@gmail.com>
2025-02-10 16:43:22 -0800

    Update user_logic.py

--- logic/confluence_logic/user_logic.py
+++ logic/confluence_logic/user_logic.py
@@ -4,3 +4,8 @@ from calls.confluence_api_calls.conf_api_users import ConfluenceUsersCalls
 class ConfGroupLogic:
     def __init__(self, is_staging=False):
         self.conf_users = ConfluenceUsersCalls(is_staging=True) if is_staging else ConfluenceUsersCalls()
+
+    def get_users_groups(self, username):
+        users_groups = self.conf_users.user_groups(username)
+        return users_groups
+    
