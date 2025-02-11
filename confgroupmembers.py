75f075f852b20b51a24c0e350ceeb5486dab0d68
mastervalor <mouradmarzouk63@gmail.com>
2025-02-10 16:11:56 -0800

    Update confgroupmembers.py

--- confgroupmembers.py
+++ confgroupmembers.py
@@ -1,17 +1,17 @@
-import json
 import csv
 import os
 from calls.confluence_api_calls.conf_api_groups import ConfluenceGroupsCalls
 
 conf_groups = ConfluenceGroupsCalls()
-pref = "/rest/api/group/app-confluence/member?expand=status"
+group = 'app-confluence'
+expand = "status"
 newFile = "app-confluence members"
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newFile), mode='w') as new_csv:
     writer = csv.writer(new_csv)
     writer.writerow(['username'])
     while True:
-        response = conf_groups.group_members(pref)
+        response = conf_groups.group_members(group, expand)
         if "next" not in response['_links']:
             break
         for member in response['results']:
