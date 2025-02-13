81b27fce65f5c99a41a8ae27e2619883b392fcc3
mastervalor <mouradmarzouk63@gmail.com>
2025-02-12 23:20:22 -0800

    Update group_logic.py

--- logic/jira_logic/group_logic.py
+++ logic/jira_logic/group_logic.py
@@ -77,29 +77,6 @@ class Groups:
             # print(startAt, maxResults)
         return members_list
 
-    def get_group_users_email(self, group):
-        start_at = 0
-        max_results = 50
-        total = 51
-        members_list = []
-        try:
-            self.jira_groups.get_group(f'?startAt={start_at}&maxResults={max_results}', group)
-            while total >= max_results:
-                response = self.jira_groups.get_group(f'?startAt={start_at}&maxResults={max_results}', group)
-                if group != 'administrators':
-                    for i in response['values']:
-                        try:
-                            members_list.append(i['emailAddress'])
-                        except KeyError:
-                            print(f'User Id {i["accountId"]} was not caught in the group grab')
-                    start_at += 50
-                    max_results += 50
-                    total = response['total']
-        except KeyError:
-            print(f"This group {group} doesn't exist")
-
-        return members_list
-
     def get_all_groups(self):
         min = 0
         max = 100
