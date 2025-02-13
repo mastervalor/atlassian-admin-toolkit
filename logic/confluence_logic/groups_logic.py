c3665829c0e7f366de48aa827a367d28a2c38e80
mastervalor <mouradmarzouk63@gmail.com>
2025-02-12 23:20:24 -0800

    Update groups_logic.py

--- logic/confluence_logic/groups_logic.py
+++ logic/confluence_logic/groups_logic.py
@@ -12,3 +12,26 @@ class ConfGroupLogic:
     def get_group_members_with_status(self, name, expanded):
         members_with_status = self.conf_groups.group_members(name, expanded)
         return members_with_status
+
+    def get_group_users_email(self, group):
+        start_at = 0
+        max_results = 50
+        total = 51
+        members_list = []
+        try:
+            self.conf_groups.get_group(f'?startAt={start_at}&maxResults={max_results}', group)
+            while total >= max_results:
+                response = self.conf_groups.get_group(f'?startAt={start_at}&maxResults={max_results}', group)
+                if group != 'administrators':
+                    for i in response['values']:
+                        try:
+                            members_list.append(i['emailAddress'])
+                        except KeyError:
+                            print(f'User Id {i["accountId"]} was not caught in the group grab')
+                    start_at += 50
+                    max_results += 50
+                    total = response['total']
+        except KeyError:
+            print(f"This group {group} doesn't exist")
+
+        return members_list
