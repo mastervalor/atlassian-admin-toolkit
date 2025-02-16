6b744fd45be5492e5908a338d50a85c60e8fdf4a
mastervalor <mouradmarzouk63@gmail.com>
2025-02-15 17:16:08 -0800

    Update group.py

--- group.py
+++ group.py
@@ -3,26 +3,6 @@ import json
 from auth import auth
 
 
-def call_group(min, max, group):
-    url = f"https://lucidmotors.atlassian.net/rest/api/3/group/member?start={min}&limit={max}"
-
-    headers = {
-        "Accept": "application/json"
-    }
-    query = {
-        'groupname': group
-    }
-    response = json.loads(requests.request(
-        "GET",
-        url,
-        headers=headers,
-        params=query,
-        auth=auth
-    ).text)
-
-    return response
-
-
 def check_group(group):
     url = f"https://lucidmotors.atlassian.net/rest/api/3/group"
 
