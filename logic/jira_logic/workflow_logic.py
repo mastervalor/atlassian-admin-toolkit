ff02df57d4941cd4e58272c9db209c2b18d51812
mastervalor <mouradmarzouk63@gmail.com>
2025-02-18 17:17:36 -0800

    Update workflow_logic.py

--- logic/jira_logic/workflow_logic.py
+++ logic/jira_logic/workflow_logic.py
@@ -5,4 +5,9 @@ class WorkflowLogic:
     def __init__(self,  is_staging=False):
         self.workflow_calls = WorkflowJiraCalls(is_staging=True) if is_staging else WorkflowJiraCalls()
 
-        
+    def delete_unused_workflow(self, name):
+
+
+    def get_workflow_id(self, workflow_name):
+        workflow = self.workflow_calls.get_workflow_by_name(workflow_name)
+        return workflow['values'][0]['id']['entityId']
