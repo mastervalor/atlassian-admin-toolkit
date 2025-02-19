961770765cb8e565c50fb0b37d2f1077b8119536
mastervalor <mouradmarzouk63@gmail.com>
2025-02-19 10:42:44 -0800

    Update delete_user_comments.py

--- process/jira_ticket_process/delete_user_comments.py
+++ process/jira_ticket_process/delete_user_comments.py
@@ -1,5 +1,3 @@
-import json
-from calls.jira_api_calls.jira_api_tickets import TicketsJiraCalls
 from logic.jira_logic.ticket_logic import Tickets
 
 ticket_logic = Tickets()
@@ -10,5 +8,7 @@ username = 'user-e5e03'
 comments = ticket_logic.get_users_comment_ids(username, ticket)
 
 print(comments)
+
+
 # print(json.dumps(comments, sort_keys=True, indent=4, separators=(",", ": ")))
 #3650988, 3650983
