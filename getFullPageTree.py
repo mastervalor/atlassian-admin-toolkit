3ed79bce6ae1b86dbb92473c44d73f9e1ad7f29e
mastervalor <mouradmarzouk63@gmail.com>
2025-02-20 22:29:20 -0800

    Delete getFullPageTree.py

--- getFullPageTree.py
+++
@@ -1,7 +0,0 @@
-from calls.confluence_api_calls.conf_api_pages import ConfluencePageCalls
-import json
-
-
-conf_pages = ConfluencePageCalls()
-page_tree = conf_pages.get_child_pages_recursive('465649108')
-print(json.dumps(page_tree, sort_keys=True, indent=4, separators=(",", ": ")))
