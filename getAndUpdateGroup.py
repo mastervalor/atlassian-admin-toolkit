f05d28ab068363ef57a170822329dfbaa6fb4f44
mastervalor <mouradmarzouk63@gmail.com>
2025-02-20 22:20:26 -0800

    Update getAndUpdateGroup.py

--- getAndUpdateGroup.py
+++ getAndUpdateGroup.py
@@ -5,33 +5,6 @@ from logic.jira_logic.group_logic import Groups
 
 group_logic = Groups()
 
-
-def put(x):
-    url = "https://lucidmotors.atlassian.net/rest/api/3/group/user"
-
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-
-    query = {
-        'groupname': 'QUAL-Manufacturing-Quality'
-    }
-
-    payload = json.dumps({
-        "accountId": x
-    })
-
-    response = requests.request(
-        "POST",
-        url,
-        data=payload,
-        headers=headers,
-        params=query,
-        auth=auth
-    )
-
-
 groups = ['QUAL-General-Factory-Paint', 'QUAL-General-Factory-BIW', 'QUAL-General-Factory-GA',
           'QUAL-General-Factory-Powertrain', 'QUAL-Incoming-Quality', 'QUAL-PDI', 'QUAL-LCPA']
 ids = []
