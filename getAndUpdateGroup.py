20903326e57b830cbe6eb89c6fbaf74f61b04b6c
mastervalor <mouradmarzouk63@gmail.com>
2025-02-20 22:26:33 -0800

    Update getAndUpdateGroup.py

--- getAndUpdateGroup.py
+++ getAndUpdateGroup.py
@@ -7,10 +7,14 @@ group_logic = Groups()
 
 groups = ['QUAL-General-Factory-Paint', 'QUAL-General-Factory-BIW', 'QUAL-General-Factory-GA',
           'QUAL-General-Factory-Powertrain', 'QUAL-Incoming-Quality', 'QUAL-PDI', 'QUAL-LCPA']
+
+account_ids = []
 ids = []
+
 for group in groups:
     response = group_logic.group_members(group)
     for value in json.loads(response)['values']:
-        put(value['accountId'])
+        account_ids.append(value['accountId'])
 
-print(ids)
+     results = group_logic.add_members_to_group(group, account_ids)
+    print(results)
