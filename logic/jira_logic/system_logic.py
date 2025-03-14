ca9d3886113959d8d53eed8eab7fffa5352bcac0
mastervalor <mouradmarzouk63@gmail.com>
2025-03-13 17:32:38 -0700

    Update system_logic.py

--- logic/jira_logic/system_logic.py
+++ logic/jira_logic/system_logic.py
@@ -1,6 +1,10 @@
-from call import Jira
+from calls.jira_api_calls.jira_api_system import SystemJiraCalls
 
 
 class Systems:
     def __init__(self):
-        self.jira = Jira()
+        self.jira_system = SystemJiraCalls()
+
+    def get_all_issue_types(self):
+        issue_types = self.jira_system.issue_types()
+        return issue_types
