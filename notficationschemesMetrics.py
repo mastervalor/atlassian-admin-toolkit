4c9456e1f48daa84afd2acdc94b9d6994f4b3331
mastervalor <mouradmarzouk63@gmail.com>
2025-03-13 18:43:00 -0700

    Update notficationschemesMetrics.py

--- notficationschemesMetrics.py
+++ notficationschemesMetrics.py
@@ -1,9 +1,7 @@
-from call import call
 from logic.jira_logic.system_logic import JiraSystemsLogic
 
 jira_system = JiraSystemsLogic()
 
-response = call('permissionscheme', 'get')
 permission_schemes = jira_system.get_all_permission_schemes()
 
 
