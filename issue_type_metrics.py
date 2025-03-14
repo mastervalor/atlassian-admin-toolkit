845c65667061a6197430aacc27778eb57aa497f2
mastervalor <mouradmarzouk63@gmail.com>
2025-03-13 17:34:07 -0700

    Update issue_type_metrics.py

--- issue_type_metrics.py
+++ issue_type_metrics.py
@@ -1,15 +1,16 @@
 from call import call
-from logic.jira_logic.
+from logic.jira_logic.system_logic import JiraSystemsLogic
 import csv
 import os
 import urllib.parse
 
 page = "issue types"
+jira_system = JiraSystemsLogic()
 
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), page), mode='w') as new_csv:
     writer = csv.writer(new_csv)
     writer.writerow(['Name', 'Ticket count'])
-    results = call('issuetype', 'get')
+    results = jira_system.get_all_issue_types()
     for i in results:
         encoded = urllib.parse.quote(i['name'])
         search = call(f'issuetype="{encoded}"', 'search')
