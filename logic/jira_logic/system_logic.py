2e0e9eee2c0179fa16dcfc2a526efe32cb3d7235
mastervalor <mouradmarzouk63@gmail.com>
2025-03-13 18:41:06 -0700

    Update system_logic.py

--- logic/jira_logic/system_logic.py
+++ logic/jira_logic/system_logic.py
@@ -8,3 +8,8 @@ class JiraSystemsLogic:
     def get_all_issue_types(self):
         issue_types = self.jira_system.issue_types()
         return issue_types
+
+    def get_all_permission_schemes(self):
+        permission_schemes = self.jira_system.permission_schemes()
+        return permission_schemes
+    
