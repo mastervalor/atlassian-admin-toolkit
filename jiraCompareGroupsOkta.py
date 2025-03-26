6bd75eebe07651b418d22420ceca5bbad571342d
mastervalor <mouradmarzouk63@gmail.com>
2025-03-26 13:09:06 -0700

    Update jiraCompareGroupsOkta.py

--- jiraCompareGroupsOkta.py
+++ jiraCompareGroupsOkta.py
@@ -11,24 +11,18 @@ newfile = "App-jira members 3"
 with open('/Users/{}/Desktop/{}.csv'.format(os.environ.get('USER'), newfile), mode='w') as new_csv:
     writer = csv.writer(new_csv)
     writer.writerow(['Email', 'App-jira in'])
-    run = 0
-    start = 0
-    total = 50
-    while start <= total:
-        group = group_logic.group_members('app-jira')
-        for member in group['values']:
-            user = okta_users.get_user_id(member['emailAddress'])
-            if user is False:
-                writer.writerow([f"Failed on {member['emailAddress']} because not groups"])
-                print(f"Failed on {member['emailAddress']} because not groups")
-                continue
-            groups = OktaUsers.user_groups(user)
-            if "app-jira" in groups:
-                writer.writerow([member['emailAddress'], "Okta"])
-                print(member['emailAddress'], "Okta")
-            else:
-                writer.writerow([member['emailAddress'], "Jira"])
-                print(member['emailAddress'], "Jira")
-        start += 50
-        print("start is currently at: ", start)
-        total = group['total']
+    group = group_logic.group_members('app-jira')
+    for member in group['values']:
+        user = okta_users.get_user_id(member['emailAddress'])
+        if user is False:
+            writer.writerow([f"Failed on {member['emailAddress']} because not groups"])
+            print(f"Failed on {member['emailAddress']} because not groups")
+            continue
+        groups = OktaUsers.user_groups(user)
+        if "app-jira" in groups:
+            writer.writerow([member['emailAddress'], "Okta"])
+            print(member['emailAddress'], "Okta")
+        else:
+            writer.writerow([member['emailAddress'], "Jira"])
+            print(member['emailAddress'], "Jira")
+    total = group['total']
