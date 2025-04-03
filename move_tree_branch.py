8900f9e5eba6b11b9e59bdaa5b3d517094dd7681
mastervalor <mouradmarzouk63@gmail.com>
2025-04-03 15:44:39 -0700

    Delete move_tree_branch.py

--- move_tree_branch.py
+++
@@ -1,6 +0,0 @@
-from calls.confluence_api_calls.conf_api_pages import ConfluencePageCalls
-import json
-
-conf_pages = ConfluencePageCalls()
-page_tree = conf_pages.get_child_pages_recursive('324733855')
-print(json.dumps(page_tree, sort_keys=True, indent=4, separators=(",", ": ")))
